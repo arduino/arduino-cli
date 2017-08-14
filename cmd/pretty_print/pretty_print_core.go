88251e52d8debd444072acef32e1eae1a7ad0c20
Alessandro Sanino <a.sanino@arduino.cc>
2017-08-14 11:02:58 +0200

    fixes #57

--- cmd/pretty_print/pretty_print_core.go
+++ cmd/pretty_print/pretty_print_core.go
@@ -58,10 +58,11 @@ func CorruptedCoreIndexFix(index cores.Index, verbosity int) (cores.StatusContex
 func coreIndexParse(index cores.Index, verbosity int) task.Wrapper {
 	ret := indexParseWrapperSkeleton()
 	ret.Task = task.Task(func() task.Result {
-		index.CreateStatusContext()
+		err := cores.LoadIndex(&index) // I try again
+		status := index.CreateStatusContext()
 		return task.Result{
-			Result: nil,
-			Error:  nil,
+			Result: status,
+			Error:  err,
 		}
 	})
 	return ret
