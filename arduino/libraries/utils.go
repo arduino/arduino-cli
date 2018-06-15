f5fa941e4014f68228a9beb4f1989af9c3bfacdc
Cristian Maglie <c.maglie@arduino.cc>
2018-06-15 12:01:46 +0200

    Added sanitizeName function

---
+++ arduino/libraries/utils.go
@@ -0,0 +1,48 @@
+/*
+ * This file is part of arduino-cli.
+ *
+ * arduino-cli is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2018 ARDUINO AG (http://www.arduino.cc/)
+ */
+
+package libraries
+
+func sanitizeName(origName string) string {
+	sanitized := ""
+	for i, c := range origName {
+		if (c >= '0' && c <= '9') ||
+			(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+			(i > 0 && c == '-') || (i > 0 && c == '.') {
+			sanitized = sanitized + string(c)
+		} else {
+			sanitized = sanitized + "_"
+		}
+	}
+
+	if len(sanitized) > 63 {
+		sanitized = sanitized[:64]
+	}
+	return sanitized
+}
