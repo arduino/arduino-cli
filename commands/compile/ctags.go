16fafda267d83fd9b8be9825195716d2d9126b36
Gianluca Varisco <g.varisco@arduino.cc>
2018-09-26 09:28:27 +0200

    Moved LibraryIndexURL over HTTPS endpoint

--- commands/compile/ctags.go
+++ commands/compile/ctags.go
@@ -33,7 +33,7 @@ func loadBuiltinCtagsMetadata(pm *packagemanager.PackageManager) {
 			OS: "i686-pc-linux-gnu",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "ctags-5.8-arduino11-pm-i686-pc-linux-gnu.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-i686-pc-linux-gnu.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-i686-pc-linux-gnu.tar.bz2",
 				Size:            106930,
 				Checksum:        "SHA-256:3e219116f54d9035f6c49c600d0bb358710dcce139798ad237de0eef7998d0e2",
 				CachePath:       "tools",
@@ -43,7 +43,7 @@ func loadBuiltinCtagsMetadata(pm *packagemanager.PackageManager) {
 			OS: "x86_64-pc-linux-gnu",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "ctags-5.8-arduino11-pm-x86_64-pc-linux-gnu.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-x86_64-pc-linux-gnu.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-x86_64-pc-linux-gnu.tar.bz2",
 				Size:            111604,
 				Checksum:        "SHA-256:62b514f3aaf37b5429ef703853bb46365fb91b4754c1916d085bf134004886e3",
 				CachePath:       "tools",
@@ -53,7 +53,7 @@ func loadBuiltinCtagsMetadata(pm *packagemanager.PackageManager) {
 			OS: "i686-mingw32",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "ctags-5.8-arduino11-pm-i686-mingw32.zip",
-				URL:             "http://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-i686-mingw32.zip",
+				URL:             "https://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-i686-mingw32.zip",
 				Size:            116455,
 				Checksum:        "SHA-256:106c9f074a3e2ec55bd8a461c1522bb4c90488275f061c3d51942862c99b8ba7",
 				CachePath:       "tools",
@@ -63,7 +63,7 @@ func loadBuiltinCtagsMetadata(pm *packagemanager.PackageManager) {
 			OS: "x86_64-apple-darwin",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "ctags-5.8-arduino11-pm-x86_64-apple-darwin.zip",
-				URL:             "http://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-x86_64-apple-darwin.zip",
+				URL:             "https://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-x86_64-apple-darwin.zip",
 				Size:            107801,
 				Checksum:        "SHA-256:e8c5db45867fb5987ad0fc429d8bbbcf5549f4b7c2b5ade863e76ac77255144d",
 				CachePath:       "tools",
@@ -73,7 +73,7 @@ func loadBuiltinCtagsMetadata(pm *packagemanager.PackageManager) {
 			OS: "arm-linux-gnueabihf",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "ctags-5.8-arduino11-pm-armv6-linux-gnueabihf.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-armv6-linux-gnueabihf.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/ctags-5.8-arduino11-pm-armv6-linux-gnueabihf.tar.bz2",
 				Size:            95271,
 				Checksum:        "SHA-256:098e8dc6a7dc0ddf09ef28e6e2e81d2ae181d12f41fb182dd78ff1387d4eb285",
 				CachePath:       "tools",
