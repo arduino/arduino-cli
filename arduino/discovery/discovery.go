7662cfad339efbdd3b5d06e688d960adb48b650b
Cristian Maglie <c.maglie@arduino.cc>
2019-01-02 16:16:32 +0100

    discovery: first implementation (WIP)

---
+++ arduino/discovery/discovery.go
@@ -0,0 +1,97 @@
+//
+// This file is part of arduino-cli.
+//
+// Copyright 2018 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to modify or
+// otherwise use the software for commercial activities involving the Arduino
+// software without disclosing the source code of your own applications. To purchase
+// a commercial license, send an email to license@arduino.cc.
+//
+
+package discovery
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	properties "github.com/arduino/go-properties-orderedmap"
+
+	"github.com/arduino/arduino-cli/executils"
+)
+
+// Discovery is an instance of a discovery tool
+type Discovery struct {
+	in      io.WriteCloser
+	out     io.ReadCloser
+	outJSON *json.Decoder
+}
+
+// BoardPort is a generic port descriptor
+type BoardPort struct {
+	Address             string          `json:"address"`
+	Label               string          `json:"label"`
+	Prefs               *properties.Map `json:"prefs"`
+	IdentificationPrefs *properties.Map `json:"identificationPrefs"`
+	Protocol            string          `json:"protocol"`
+	ProtocolLabel       string          `json:"protocolLabel"`
+}
+
+type eventJSON struct {
+	EventType string       `json:"eventType,required"`
+	Ports     []*BoardPort `json:"ports"`
+}
+
+// NewFromCommandLine creates a new Discovery object
+func NewFromCommandLine(args ...string) (*Discovery, error) {
+	cmd, err := executils.Command(args)
+	if err != nil {
+		return nil, fmt.Errorf("creating discovery process: %s", err)
+	}
+	disc := &Discovery{}
+	if in, err := cmd.StdinPipe(); err == nil {
+		disc.in = in
+	} else {
+		return nil, fmt.Errorf("creating stdin pipe for discovery: %s", err)
+	}
+	if out, err := cmd.StdoutPipe(); err == nil {
+		disc.out = out
+		disc.outJSON = json.NewDecoder(disc.out)
+	} else {
+		return nil, fmt.Errorf("creating stdout pipe for discovery: %s", err)
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("starting discovery: %s", err)
+	}
+	return disc, nil
+}
+
+// List retrieve the port list from this discovery
+func (d *Discovery) List() ([]*BoardPort, error) {
+	if _, err := d.in.Write([]byte("LIST\n")); err != nil {
+		return nil, fmt.Errorf("sending LIST command to discovery: %s", err)
+	}
+	var event eventJSON
+	if err := d.outJSON.Decode(&event); err != nil {
+		return nil, fmt.Errorf("decoding LIST command: %s", err)
+	}
+	return event.Ports, nil
+}
+
+// Close stops the Discovery and free the resources
+func (d *Discovery) Close() error {
+	if err := d.in.Close(); err != nil {
+		return fmt.Errorf("closing stdin pipe: %s", err)
+	}
+	if err := d.out.Close(); err != nil {
+		return fmt.Errorf("closing stdout pipe: %s", err)
+	}
+	return nil
+}
