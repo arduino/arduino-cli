76198dab09ce094673d0982cb3d418950a7e04fc
Cristian Maglie <c.maglie@arduino.cc>
2019-03-01 09:43:56 +0100

    Better client example

--- daemon/client/client.go
+++ daemon/client/client.go
@@ -4,32 +4,63 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/arduino/arduino-cli/rpc"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please specify Arduino DATA_DIR as first argument")
+		os.Exit(1)
+	}
+	datadir := os.Args[1]
 	conn, err := grpc.Dial("127.0.0.1:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	client := rpc.NewArduinoCoreClient(conn)
 
-	resp, err := client.Init(context.Background(), &rpc.InitReq{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(resp.GetInstance())
-
-	details, err := client.BoardDetails(context.Background(), &rpc.BoardDetailsReq{
-		Instance: resp.GetInstance(),
-		Fqbn:     "",
+	resp, err := client.Init(context.Background(), &rpc.InitReq{
+		Configuration: &rpc.Configuration{
+			DataDir: datadir,
+		},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(details.GetName())
+	if resp.GetResult().GetFailed() {
+		fmt.Println("Error opening server instance:", resp.GetResult().GetMessage())
+		os.Exit(1)
+	}
+	instance := resp.GetInstance()
+	fmt.Println("Opened new server instance:", instance)
+
+	details, err := client.BoardDetails(context.Background(), &rpc.BoardDetailsReq{
+		Instance: instance,
+		Fqbn:     "arduino:samd:mkr1000",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if details.GetResult().GetFailed() {
+		fmt.Println("Error getting board data:", details.GetResult().GetMessage())
+	} else {
+		fmt.Println("Board name: ", details.GetName())
+	}
+
+	destroyResp, err := client.Destroy(context.Background(), &rpc.DestroyReq{
+		Instance: instance,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if destroyResp.GetResult().GetFailed() {
+		fmt.Println("Error closing instance:", destroyResp.GetResult().GetMessage())
+	} else {
+		fmt.Println("Successfully closed server instance")
+	}
 	/*
 		compile, err := client.Compile(context.Background(), &pb.CompileReq{})
 		if err != nil {
