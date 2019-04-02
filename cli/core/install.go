8e7db44fb0e2b6432b7e00110d51bc98f7019aeb
Riccardo Rizzo <r.rizzo@arduino.cc>
2019-04-02 11:29:41 +0200

    Moved "PlatformInstall" implementation from cli to commands (WIP)
    - Renamed install in stall platform;
    - Regenerated the Pb file from core.proto;
    - Modified demon callback;

--- cli/core/install.go
+++ cli/core/install.go
@@ -18,12 +18,11 @@
 package core
 
 import (
-	"os"
+	"context"
 
-	"github.com/arduino/arduino-cli/arduino/cores"
-	"github.com/arduino/arduino-cli/arduino/cores/packagemanager"
 	"github.com/arduino/arduino-cli/cli"
-	"github.com/arduino/arduino-cli/common/formatter"
+	"github.com/arduino/arduino-cli/commands/core"
+	"github.com/arduino/arduino-cli/rpc"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -44,117 +43,24 @@ func initInstallCommand() *cobra.Command {
 }
 
 func runInstallCommand(cmd *cobra.Command, args []string) {
+	instance := cli.CreateInstance()
 	logrus.Info("Executing `arduino core download`")
 
 	platformsRefs := parsePlatformReferenceArgs(args)
-	pm, _ := cli.InitPackageAndLibraryManagerWithoutBundles()
 
 	for _, platformRef := range platformsRefs {
-		installPlatformByRef(pm, platformRef)
+		core.PlatformInstall(context.Background(), &rpc.PlatformInstallReq{
+			Instance:        instance,
+			PlatformPackage: platformRef.Package,
+			Architecture:    platformRef.PlatformArchitecture,
+			Version:         platformRef.PlatformVersion.String(),
+		})
+		// if err != nil {
+		// 	formatter.PrintError(err, "Error during build")
+		// 	os.Exit(cli.ErrGeneric)
+		// }
+		// }
 	}
 
 	// TODO: Cleanup unused tools
-}
-
-func installPlatformByRef(pm *packagemanager.PackageManager, platformRef *packagemanager.PlatformReference) {
-	platform, tools, err := pm.FindPlatformReleaseDependencies(platformRef)
-	if err != nil {
-		formatter.PrintError(err, "Could not determine platform dependencies")
-		os.Exit(cli.ErrBadCall)
-	}
-
-	installPlatform(pm, platform, tools)
-}
-
-func installPlatform(pm *packagemanager.PackageManager, platformRelease *cores.PlatformRelease, requiredTools []*cores.ToolRelease) {
-	log := pm.Log.WithField("platform", platformRelease)
-
-	// Prerequisite checks before install
-	if platformRelease.IsInstalled() {
-		log.Warn("Platform already installed")
-		formatter.Print("Platform " + platformRelease.String() + " already installed")
-		return
-	}
-	toolsToInstall := []*cores.ToolRelease{}
-	for _, tool := range requiredTools {
-		if tool.IsInstalled() {
-			log.WithField("tool", tool).Warn("Tool already installed")
-			formatter.Print("Tool " + tool.String() + " already installed")
-		} else {
-			toolsToInstall = append(toolsToInstall, tool)
-		}
-	}
-
-	// Package download
-	for _, tool := range toolsToInstall {
-		downloadTool(pm, tool)
-	}
-	downloadPlatform(pm, platformRelease)
-
-	for _, tool := range toolsToInstall {
-		InstallToolRelease(pm, tool)
-	}
-
-	// Are we installing or upgrading?
-	platform := platformRelease.Platform
-	installed := pm.GetInstalledPlatformRelease(platform)
-	if installed == nil {
-		log.Info("Installing platform")
-		formatter.Print("Installing " + platformRelease.String() + "...")
-	} else {
-		log.Info("Updating platform " + installed.String())
-		formatter.Print("Updating " + installed.String() + " with " + platformRelease.String() + "...")
-	}
-
-	// Install
-	err := pm.InstallPlatform(platformRelease)
-	if err != nil {
-		log.WithError(err).Error("Cannot install platform")
-		formatter.PrintError(err, "Cannot install platform")
-		os.Exit(cli.ErrGeneric)
-	}
-
-	// If upgrading remove previous release
-	if installed != nil {
-		err := pm.UninstallPlatform(installed)
-
-		// In case of error try to rollback
-		if err != nil {
-			log.WithError(err).Error("Error updating platform.")
-			formatter.PrintError(err, "Error updating platform")
-
-			// Rollback
-			if err := pm.UninstallPlatform(platformRelease); err != nil {
-				log.WithError(err).Error("Error rolling-back changes.")
-				formatter.PrintError(err, "Error rolling-back changes.")
-			}
-			os.Exit(cli.ErrGeneric)
-		}
-	}
-
-	log.Info("Platform installed")
-	formatter.Print(platformRelease.String() + " installed")
-}
-
-// InstallToolRelease installs a ToolRelease
-func InstallToolRelease(pm *packagemanager.PackageManager, toolRelease *cores.ToolRelease) {
-	log := pm.Log.WithField("Tool", toolRelease)
-
-	if toolRelease.IsInstalled() {
-		log.Warn("Tool already installed")
-		formatter.Print("Tool " + toolRelease.String() + " already installed")
-		return
-	}
-
-	log.Info("Installing tool")
-	formatter.Print("Installing " + toolRelease.String() + "...")
-	err := pm.InstallTool(toolRelease)
-	if err != nil {
-		log.WithError(err).Warn("Cannot install tool")
-		formatter.PrintError(err, "Cannot install tool: "+toolRelease.String())
-		os.Exit(cli.ErrGeneric)
-	}
-
-	log.Info("Tool installed")
-	formatter.Print(toolRelease.String() + " installed")
 }
