b95af45154bb61d56140dd89166d1986614c9faa
Massimiliano Pippi <mpippi@gmail.com>
2019-06-03 18:10:31 +0200

    update tools to v1.0

--- commands/bundled_tools_serial_discovery.go
+++ commands/bundled_tools_serial_discovery.go
@@ -34,56 +34,66 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 	ctagsTool := builtinPackage.GetOrCreateTool("serial-discovery")
 	ctagsRel := ctagsTool.GetOrCreateRelease(serialDiscoveryVersion)
 	ctagsRel.Flavors = []*cores.Flavor{
-		// {
-		// 	OS: "i686-pc-linux-gnu",
-		// 	Resource: &resources.DownloadResource{
-		// 		ArchiveFileName: "serial-discovery-1.0.0-i686-pc-linux-gnu.tar.bz2",
-		// 		URL:             "https://downloads.arduino.cc/tools/serial-discovery-1.0.0-i686-pc-linux-gnu.tar.bz2",
-		// 		Size:            ,
-		// 		Checksum:        "SHA-256:",
-		// 		CachePath:       "tools",
-		// 	},
-		// },
 		{
-			OS: "x86_64-pc-linux-gnu",
+			OS: "i686-pc-linux-gnu",
 			Resource: &resources.DownloadResource{
-				ArchiveFileName: "serial-discovery-0.5.0-x86_64-pc-linux-gnu.tar.bz2",
-				URL:             "https://downloads.arduino.cc/tools/serial-discovery-0.5.0-x86_64-pc-linux-gnu.tar.bz2",
-				Size:            1507380,
-				Checksum:        "SHA-256:473cdd9e9f189cfd507b1f6c312d767513da11ec87cdbff1610153d6285e15ce",
+				ArchiveFileName: "serial-discovery-linux32-v1.0.0.tar.bz2",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linux32-v1.0.0.tar.bz2",
+				Size:            1469113,
+				Checksum:        "SHA-256:35d96977844ad8d5ca9363e1ae5794450e5f7cf3d29ce7fdfe656b59e7fff725",
 				CachePath:       "tools",
 			},
 		},
-		// {
-		// 	OS: "i686-mingw32",
-		// 	Resource: &resources.DownloadResource{
-		// 		ArchiveFileName: "serial-discovery-1.0.0-i686-mingw32.zip",
-		// 		URL:             "https://downloads.arduino.cc/tools/serial-discovery-1.0.0-i686-mingw32.zip",
-		// 		Size:            ,
-		// 		Checksum:        "SHA-256:",
-		// 		CachePath:       "tools",
-		// 	},
-		// },
-		// {
-		// 	OS: "x86_64-apple-darwin",
-		// 	Resource: &resources.DownloadResource{
-		// 		ArchiveFileName: "serial-discovery-1.0.0-x86_64-apple-darwin.zip",
-		// 		URL:             "https://downloads.arduino.cc/tools/serial-discovery-1.0.0-x86_64-apple-darwin.zip",
-		// 		Size:            ,
-		// 		Checksum:        "SHA-256:",
-		// 		CachePath:       "tools",
-		// 	},
-		// },
-		// {
-		// 	OS: "arm-linux-gnueabihf",
-		// 	Resource: &resources.DownloadResource{
-		// 		ArchiveFileName: "serial-discovery-1.0.0-armv6-linux-gnueabihf.tar.bz2",
-		// 		URL:             "https://downloads.arduino.cc/tools/serial-discovery-1.0.0-armv6-linux-gnueabihf.tar.bz2",
-		// 		Size:            ,
-		// 		Checksum:        "SHA-256:",
-		// 		CachePath:       "tools",
-		// 	},
-		// },
+		{
+			OS: "x86_64-pc-linux-gnu",
+			Resource: &resources.DownloadResource{
+				ArchiveFileName: "serial-discovery-linux64-v1.0.0.tar.bz2",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linux64-v1.0.0.tar.bz2",
+				Size:            1503971,
+				Checksum:        "SHA-256:1a870d4d823ea6ebec403f63b10a1dbc9c623a6efea5cfa9141fa20045b731e2",
+				CachePath:       "tools",
+			},
+		},
+		{
+			OS: "i686-mingw32",
+			Resource: &resources.DownloadResource{
+				ArchiveFileName: "serial-discovery-windows-v1.0.0.zip",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-windows-v1.0.0.zip",
+				Size:            1512379,
+				Checksum:        "SHA-256:b956128ab27a3a883c938d17cad640ba396876472f2ed25d8e661f12f5d0f584",
+				CachePath:       "tools",
+			},
+		},
+		{
+			OS: "x86_64-apple-darwin",
+			Resource: &resources.DownloadResource{
+				ArchiveFileName: "serial-discovery-macosx-v1.0.0.tar.bz2",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-macosx-v1.0.0.tar.bz2",
+				Size:            746132,
+				Checksum:        "SHA-256:fcff1b972b70a73cd738facc6d99174d8323293b60c12149c8f6f3084fb2170e",
+				CachePath:       "tools",
+			},
+		},
+		{
+			OS: "arm-linux-gnueabihf",
+			Resource: &resources.DownloadResource{
+				ArchiveFileName: "serial-discovery-linuxarm-v1.0.0.tar.bz2",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linuxarm-v1.0.0.tar.bz2",
+				Size:            1395174,
+				Checksum:        "SHA-256:f196765caa62d38475208c27b3b516e61427d5d3a8ddc6e863acb4e4a3984701",
+				CachePath:       "tools",
+			},
+		},
+		{
+			OS: "arm64-linux-gnueabihf",
+			Resource: &resources.DownloadResource{
+				ArchiveFileName: "serial-discovery-linuxarm64-v1.0.0.tar.bz2",
+				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linuxarm64-v1.0.0.tar.bz2",
+				Size:            1402706,
+				Checksum:        "SHA-256:c87010ed670254c06ac7abbc4daf7446e4e17f1945a75fc2602dd5930835dd25",
+				CachePath:       "tools",
+			},
+		},
 	}
 }
 
