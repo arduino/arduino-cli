d8a36c5429c60ed19e2f5935c4929f6365c0ee73
Massimiliano Pippi <mpippi@gmail.com>
2019-06-03 18:24:01 +0200

    use https urls

--- commands/bundled_tools_serial_discovery.go
+++ commands/bundled_tools_serial_discovery.go
@@ -38,7 +38,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "i686-pc-linux-gnu",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-linux32-v1.0.0.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linux32-v1.0.0.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-linux32-v1.0.0.tar.bz2",
 				Size:            1469113,
 				Checksum:        "SHA-256:35d96977844ad8d5ca9363e1ae5794450e5f7cf3d29ce7fdfe656b59e7fff725",
 				CachePath:       "tools",
@@ -48,7 +48,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "x86_64-pc-linux-gnu",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-linux64-v1.0.0.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linux64-v1.0.0.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-linux64-v1.0.0.tar.bz2",
 				Size:            1503971,
 				Checksum:        "SHA-256:1a870d4d823ea6ebec403f63b10a1dbc9c623a6efea5cfa9141fa20045b731e2",
 				CachePath:       "tools",
@@ -58,7 +58,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "i686-mingw32",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-windows-v1.0.0.zip",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-windows-v1.0.0.zip",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-windows-v1.0.0.zip",
 				Size:            1512379,
 				Checksum:        "SHA-256:b956128ab27a3a883c938d17cad640ba396876472f2ed25d8e661f12f5d0f584",
 				CachePath:       "tools",
@@ -68,7 +68,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "x86_64-apple-darwin",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-macosx-v1.0.0.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-macosx-v1.0.0.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-macosx-v1.0.0.tar.bz2",
 				Size:            746132,
 				Checksum:        "SHA-256:fcff1b972b70a73cd738facc6d99174d8323293b60c12149c8f6f3084fb2170e",
 				CachePath:       "tools",
@@ -78,7 +78,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "arm-linux-gnueabihf",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-linuxarm-v1.0.0.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linuxarm-v1.0.0.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-linuxarm-v1.0.0.tar.bz2",
 				Size:            1395174,
 				Checksum:        "SHA-256:f196765caa62d38475208c27b3b516e61427d5d3a8ddc6e863acb4e4a3984701",
 				CachePath:       "tools",
@@ -88,7 +88,7 @@ func loadBuiltinSerialDiscoveryMetadata(pm *packagemanager.PackageManager) {
 			OS: "arm64-linux-gnueabihf",
 			Resource: &resources.DownloadResource{
 				ArchiveFileName: "serial-discovery-linuxarm64-v1.0.0.tar.bz2",
-				URL:             "http://downloads.arduino.cc/tools/serial-discovery-linuxarm64-v1.0.0.tar.bz2",
+				URL:             "https://downloads.arduino.cc/tools/serial-discovery-linuxarm64-v1.0.0.tar.bz2",
 				Size:            1402706,
 				Checksum:        "SHA-256:c87010ed670254c06ac7abbc4daf7446e4e17f1945a75fc2602dd5930835dd25",
 				CachePath:       "tools",
