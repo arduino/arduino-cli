a2f411739ad09334f58ca58f82c9c0b8e390f94f
Roberto Sora <r.sora@arduino.cc>
2019-06-04 18:48:56 +0200

    add testing for user agent-string generation

---
+++ arduino/resources/helpers_test.go
@@ -0,0 +1,84 @@
+/*
+ * This file is part of arduino-cli.
+ *
+ * Copyright 2018 ARDUINO SA (http://www.arduino.cc/)
+ *
+ * This software is released under the GNU General Public License version 3,
+ * which covers the main part of arduino-cli.
+ * The terms of this license can be found at:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * You can be released from the requirements of the above licenses by purchasing
+ * a commercial license. Buying such a license is mandatory if you want to modify or
+ * otherwise use the software for commercial activities involving the Arduino
+ * software without disclosing the source code of your own applications. To purchase
+ * a commercial license, send an email to license@arduino.cc.
+ */
+
+package resources
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/arduino/arduino-cli/global"
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/require"
+)
+
+type EchoHandler struct{}
+
+// EchoHandler echos back the request as a response if used as http handler
+func (h *EchoHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	request.Write(writer)
+}
+
+func TestDownloadApplyUserAgentHeaderUsingConfig(t *testing.T) {
+	goldUserAgentString := "User-Agent: " + fmt.Sprintf("%s/%s (%s; %s; %s) Commit:%s/Build:%s", global.GetApplication(),
+		global.GetVersion(), runtime.GOARCH, runtime.GOOS, runtime.Version(), global.GetCommit(), global.GetBuildDate())
+
+	tmp, err := paths.MkTempDir("", "")
+	require.NoError(t, err)
+	defer tmp.RemoveAll()
+
+	// startup echo server
+	srv := httptest.NewServer(&EchoHandler{})
+	defer srv.Close()
+
+	r := &DownloadResource{
+		ArchiveFileName: "echo.txt",
+		CachePath:       "cache",
+		URL:             srv.URL,
+	}
+
+	d, err := r.Download(tmp)
+	require.NoError(t, err)
+	err = d.Run()
+	require.NoError(t, err)
+
+	// leverage the download helper to download the echo for the request made by the downloader itself
+	//
+	// expect something like:
+	//    GET /echo HTTP/1.1
+	//    Host: 127.0.0.1:64999
+	//    User-Agent: arduino-cli/0.3.6-alpha.preview (amd64; linux; go1.10) Commit:missing/Build:missing
+	//    Accept-Encoding: gzip
+
+	b, err := ioutil.ReadFile(tmp.String() + "/cache/echo.txt") // just pass the file name
+	require.NoError(t, err)
+
+	requestLines := strings.Split(string(b), "\r\n")
+	userAgentHeaderString := ""
+	for _, line := range requestLines {
+		if strings.Contains(line, "User-Agent: ") {
+			userAgentHeaderString = line
+		}
+	}
+	require.Equal(t, goldUserAgentString, userAgentHeaderString)
+
+}
