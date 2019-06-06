c1196a5d75e949fe149e0cb523b6e085243870e2
Roberto Sora <r.sora@arduino.cc>
2019-06-06 10:45:52 +0200

    refactor versioning variables in version package and implement Info struct wihth String method

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@
  *
  * Copyright 2018 ARDUINO SA (http://www.arduino.cc/)
  *
- * This software is released under the GNU General Public License version 3,
+ * This software is released under the GNU General Public License defaultVersionString 3,
  * which covers the main part of arduino-cli.
  * The terms of this license can be found at:
  * https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -18,20 +18,21 @@
 package global
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 // appName is the command line name of the Arduino CLI executable on the user system (users may change it)
 var appName = filepath.Base(os.Args[0])
 
 var (
-	application = "arduino-cli"
-	version     = "0.3.6-alpha.preview"
-	commit      = "missing"
-	cvsRef      = "missing"
-	buildDate   = "missing"
-	repository  = "missing"
+	application          = "arduino-cli"
+	defaultVersionString = "0.3.6-alpha.preview"
+	versionString        = ""
+	commit               = "missing"
+	buildDate            = time.Time{}
 )
 
 func GetAppName() string {
@@ -43,30 +44,40 @@ func GetApplication() string {
 }
 
 func GetVersion() string {
-	return version
+	return defaultVersionString
 }
 
 func GetCommit() string {
 	return commit
 }
 
-func GetCvsRef() string {
-	return cvsRef
-}
-
 func GetBuildDate() string {
-	return buildDate
-}
-
-func GetRepository() string {
-	return repository
+	return ""
 }
 
 type Info struct {
-	Application string `json:"application"`
-	Version     string `json:"version"`
-	Commit      string `json:"commit"`
-	CvsRef      string `json:"cvsRef"`
-	BuildDate   string `json:"buildDate"`
-	Repository  string `json:"repository"`
+	Application   string    `json:"Application"`
+	VersionString string    `json:"VersionString"`
+	Commit        string    `json:"Commit"`
+	BuildDate     time.Time `json:"BuildDate"`
+}
+
+func NewInfo(application string, versionString string, commit string) *Info {
+	return &Info{
+		Application:   application,
+		VersionString: versionString,
+		Commit:        commit,
+		BuildDate:     buildDate,
+	}
+}
+
+func (i *Info) String() string {
+	return fmt.Sprintf("%s Version: %s Commit: %s BuildDate: %s", i.Application, i.VersionString, i.Commit, i.BuildDate)
+}
+
+func init() {
+	if versionString == "" {
+		versionString = defaultVersionString
+	}
+	buildDate = time.Now().UTC()
 }
