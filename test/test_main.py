5d0c4f83aa05dcc5e3eba2961caac1bc5591842b
Luca Cipriani <luca.opensource@gmail.com>
2019-07-15 10:23:03 +0200

    Explicit cli path as a variable

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,14 @@
-from invoke import run,Responder
+from invoke import run, Responder
+import os
 
-#from pytest import capsys
-
+this_test_path = os.path.dirname(os.path.realpath(__file__))
+cli_path = os.path.join(this_test_path, '..', 'arduino-cli')
 
 def run_help():
-    help = run('arduino-cli help', pty=True)
+    help = run(cli_path + ' help', pty=True)
     return help
 
 
 def test_command_help():
-    print(func())
+    print(run_help())
     assert True == True
