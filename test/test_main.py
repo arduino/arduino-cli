c3e4c0d307c771b695def487106f5aeb96898dd5
Luca Cipriani <luca.opensource@gmail.com>
2019-07-15 09:48:35 +0200

    Added proper pytest.ini to remove all warnings/errors not from tests

--- test/test_main.py
+++ test/test_main.py
@@ -3,14 +3,10 @@ from invoke import run,Responder
 #from pytest import capsys
 
 
-def func(capfd):
-    # help = run('arduino-cli help', hide=True, echo=False)
-    # capture = py.io.StdCaptureFD(out=False, in_=False )
-    #with capfd.disabled():
+def func():
     help = run('arduino-cli help', pty=True)
-    # out,err = capture.reset()
     return help
 
-def test_command_help(capfd):
-    print(func(capfd))
+def test_command_help():
+    print(func())
     assert True == True
