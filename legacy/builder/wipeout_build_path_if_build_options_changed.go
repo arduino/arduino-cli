e26f82fa327fa6a53a3772b47c4296895e3cc797
Massimiliano Pippi <mpippi@gmail.com>
2019-08-08 15:22:28 +0200

    remove noisy line

--- legacy/builder/wipeout_build_path_if_build_options_changed.go
+++ legacy/builder/wipeout_build_path_if_build_options_changed.go
@@ -79,7 +79,9 @@ func (s *WipeoutBuildPathIfBuildOptionsChanged) Run(ctx *types.Context) error {
 		}
 	}
 
-	logger.Println(constants.LOG_LEVEL_INFO, constants.MSG_BUILD_OPTIONS_CHANGED)
+	// FIXME: this should go outside legacy and behind a `logrus` call so users can
+	// control when this should be printed.
+	// logger.Println(constants.LOG_LEVEL_INFO, constants.MSG_BUILD_OPTIONS_CHANGED)
 
 	buildPath := ctx.BuildPath
 	files, err := gohasissues.ReadDir(buildPath.String())
