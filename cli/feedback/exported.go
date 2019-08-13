ffc78a5c65d73d51d893d53e5f66f18c6ba2748b
Massimiliano Pippi <m.pippi@arduino.cc>
2019-08-13 13:13:53 +0200

    add a simple wrapper to report user messages

---
+++ cli/feedback/exported.go
@@ -0,0 +1,58 @@
+// This file is part of arduino-cli.
+//
+// Copyright 2019 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to modify or
+// otherwise use the software for commercial activities involving the Arduino
+// software without disclosing the source code of your own applications. To purchase
+// a commercial license, send an email to license@arduino.cc.
+
+package feedback
+
+import (
+	"io"
+)
+
+var (
+	fb = DefaultFeedback()
+)
+
+// OutputWriter returns the underlying io.Writer to be used when the Print*
+// api is not enough
+func OutputWriter() io.Writer {
+	return fb.OutputWriter()
+}
+
+// ErrorWriter is the same as OutputWriter but exposes the underlying error
+// writer
+func ErrorWriter() io.Writer {
+	return fb.ErrorWriter()
+}
+
+// Printf behaves like fmt.Printf but writes on the out writer
+func Printf(format string, v ...interface{}) {
+	fb.Printf(format, v...)
+}
+
+// Print behaves like fmt.Print but writes on the out writer
+func Print(v ...interface{}) {
+	fb.Print(v...)
+}
+
+// Errorf behaves like fmt.Printf but writes on the error writer. It also logs
+// the error.
+func Errorf(format string, v ...interface{}) {
+	fb.Errorf(format, v...)
+}
+
+// Error behaves like fmt.Print but writes on the error writer. It also logs
+// the error.
+func Error(v ...interface{}) {
+	fb.Error(v...)
+}
---
+++ cli/feedback/feedback.go
@@ -0,0 +1,75 @@
+// This file is part of arduino-cli.
+//
+// Copyright 2019 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to modify or
+// otherwise use the software for commercial activities involving the Arduino
+// software without disclosing the source code of your own applications. To purchase
+// a commercial license, send an email to license@arduino.cc.
+
+package feedback
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Feedback wraps an io.Writer and provides an uniform API the CLI can use to
+// provide feedback to the users.
+type Feedback struct {
+	out io.Writer
+	err io.Writer
+}
+
+// DefaultFeedback provides a basic feedback object to be used as default.
+func DefaultFeedback() *Feedback {
+	return &Feedback{
+		out: os.Stdout,
+		err: os.Stderr,
+	}
+}
+
+// OutputWriter returns the underlying io.Writer to be used when the Print*
+// api is not enough.
+func (fb *Feedback) OutputWriter() io.Writer {
+	return fb.out
+}
+
+// ErrorWriter is the same as OutputWriter but exposes the underlying error
+// writer.
+func (fb *Feedback) ErrorWriter() io.Writer {
+	return fb.out
+}
+
+// Printf behaves like fmt.Printf but writes on the out writer
+func (fb *Feedback) Printf(format string, v ...interface{}) {
+	fmt.Fprintf(fb.out, format, v...)
+}
+
+// Print behaves like fmt.Print but writes on the out writer.
+func (fb *Feedback) Print(v ...interface{}) {
+	fmt.Fprint(fb.out, v...)
+}
+
+// Errorf behaves like fmt.Printf but writes on the error writer. It also logs
+// the error.
+func (fb *Feedback) Errorf(format string, v ...interface{}) {
+	fmt.Fprintf(fb.err, format, v...)
+	logrus.Errorf(fmt.Sprintf(format, v...))
+}
+
+// Error behaves like fmt.Print but writes on the error writer. It also logs
+// the error.
+func (fb *Feedback) Error(v ...interface{}) {
+	fmt.Fprint(fb.err, v...)
+	logrus.Error(fmt.Sprint(v...))
+}
