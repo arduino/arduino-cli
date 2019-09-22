7403a3930d59401cc332cbebcef4c92f58576ce7
david gauchard <gauchard@laas.fr>
2019-09-22 23:32:20 +0200

    retrigger CI

--- arduino/builder/sketch.go
+++ arduino/builder/sketch.go
@@ -60,6 +60,7 @@ func SketchSaveItemCpp(item *sketch.Item, destPath string) error {
 
 // SimpleLocalWalk locally replaces filepath.Walk and/but goes through symlinks
 func SimpleLocalWalk(root string, walkFn func(path string, info os.FileInfo, err error) error) error {
+
 	info, err := os.Stat(root)
 
 	if err != nil {
