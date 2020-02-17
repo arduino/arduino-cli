73fec49bf3236a3ad36f1fd465d43c0c9f64e8d9
Cristian Maglie <c.maglie@arduino.cc>
2020-02-17 15:43:17 +0100

    Working stdio streaming

--- commands/daemon/debug.go
+++ commands/daemon/debug.go
@@ -16,11 +16,10 @@
 package daemon
 
 import (
-	"context"
 	"fmt"
+
 	cmd "github.com/arduino/arduino-cli/commands/debug"
 	dbg "github.com/arduino/arduino-cli/rpc/debug"
-	"io"
 )
 
 // DebugService implements the `Debug` service
@@ -44,98 +43,11 @@ func (s *DebugService) StreamingOpen(stream dbg.Debug_StreamingOpenServer) error
 	}
 
 	// launch debug recipe attaching stdin and out to grpc streaming
-	cmd, err := cmd.Debug(context.Background(), req)
+	resp, err := cmd.Debug(stream.Context(), req, stream, feedStream(func(data []byte) {
+		stream.Send(&dbg.StreamingOpenResp{Data: data})
+	}))
 	if err != nil {
 		return (err)
 	}
-	in, err := cmd.StdinPipe()
-	if err != nil {
-		return (err)
-	}
-	defer in.Close()
-
-	out, err := cmd.StdoutPipe()
-	if err != nil {
-		return (err)
-	}
-	defer out.Close()
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println("%v\n", err)
-		return err
-	}
-
-	// we'll use these channels to communicate with the goroutines
-	// handling the stream and the target respectively
-	streamClosed := make(chan error)
-	targetClosed := make(chan error)
-
-	// now we can read the other commands and re-route to the Debug Client...
-	go func() {
-		for {
-			command, err := stream.Recv()
-			if err == io.EOF {
-				// stream was closed
-				streamClosed <- nil
-				break
-			}
-
-			if err != nil {
-				// error reading from stream
-				streamClosed <- err
-				break
-			}
-
-			if _, err := in.Write(command.GetData()); err != nil {
-				// error writing to target
-				targetClosed <- err
-				break
-			}
-		}
-	}()
-
-	// ...and read from the Debug and forward to the output stream
-	go func() {
-		buf := make([]byte, 8)
-		for {
-			n, err := out.Read(buf)
-			if err != nil {
-				// error reading from target
-				targetClosed <- err
-				break
-			}
-
-			if n == 0 {
-				// target was closed
-				targetClosed <- nil
-				break
-			}
-
-			err = stream.Send(&dbg.StreamingOpenResp{
-				Data: buf[:n],
-			})
-			if err != nil {
-				// error sending to stream
-				streamClosed <- err
-				break
-			}
-		}
-	}()
-
-	// let goroutines route messages from/to the Debug
-	// until either the client closes the stream or the
-	// Debug target is closed
-	for {
-		select {
-		case err := <-streamClosed:
-			fmt.Println("streamClosed")
-			cmd.Process.Kill()
-			cmd.Wait()
-			return err
-		case err := <-targetClosed:
-			fmt.Println("targetClosed")
-			return err
-		}
-	}
+	return stream.Send(resp)
 }
--- commands/debug/debug.go
+++ commands/debug/debug.go
@@ -18,158 +18,14 @@ package debug
 import (
 	"context"
 	"fmt"
-	"github.com/arduino/arduino-cli/arduino/cores"
-	"github.com/arduino/arduino-cli/arduino/sketches"
-	"github.com/arduino/arduino-cli/commands"
+	"io"
+
 	"github.com/arduino/arduino-cli/executils"
 	dbg "github.com/arduino/arduino-cli/rpc/debug"
-	"github.com/arduino/go-paths-helper"
-	"github.com/arduino/go-properties-orderedmap"
-	"github.com/sirupsen/logrus"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
 // Debug FIXMEDOC
-func Debug(ctx context.Context, req *dbg.DebugReq, in func(data []bytes), out func(data []bytes)) (*exec.Cmd, error) {
-	//logrus.Tracef("Debug %s on %s started", req.GetSketchPath(), req.GetFqbn())
-
-	//// TODO: make a generic function to extract sketch from request
-	// and remove duplication in commands/compile.go
-	if req.GetSketchPath() == "" {
-		return fmt.Errorf("missing sketchPath")
-	}
-	sketchPath := paths.New(req.GetSketchPath())
-	sketch, err := sketches.NewSketchFromPath(sketchPath)
-	if err != nil {
-		return fmt.Errorf("opening sketch: %s", err)
-	}
-
-	// FIXME: make a specification on how a port is specified via command line
-	port := req.GetPort()
-	if port == "" {
-		return fmt.Errorf("no upload port provided")
-	}
-
-	fqbnIn := req.GetFqbn()
-	if fqbnIn == "" && sketch != nil && sketch.Metadata != nil {
-		fqbnIn = sketch.Metadata.CPU.Fqbn
-	}
-	if fqbnIn == "" {
-		return fmt.Errorf("no Fully Qualified Board Name provided")
-	}
-	fqbn, err := cores.ParseFQBN(fqbnIn)
-	if err != nil {
-		return fmt.Errorf("incorrect FQBN: %s", err)
-	}
-
-	pm := commands.GetPackageManager(req.GetInstance().GetId())
-
-	// Find target board and board properties
-	_, _, board, boardProperties, _, err := pm.ResolveFQBN(fqbn)
-	if err != nil {
-		return fmt.Errorf("incorrect FQBN: %s", err)
-	}
-
-	// Load programmer tool
-	uploadToolPattern, have := boardProperties.GetOk("upload.tool")
-	if !have || uploadToolPattern == "" {
-		return fmt.Errorf("cannot get programmer tool: undefined 'upload.tool' property")
-	}
-
-	var referencedPlatformRelease *cores.PlatformRelease
-	if split := strings.Split(uploadToolPattern, ":"); len(split) > 2 {
-		return fmt.Errorf("invalid 'upload.tool' property: %s", uploadToolPattern)
-	} else if len(split) == 2 {
-		referencedPackageName := split[0]
-		uploadToolPattern = split[1]
-		architecture := board.PlatformRelease.Platform.Architecture
-
-		if referencedPackage := pm.Packages[referencedPackageName]; referencedPackage == nil {
-			return fmt.Errorf("required platform %s:%s not installed", referencedPackageName, architecture)
-		} else if referencedPlatform := referencedPackage.Platforms[architecture]; referencedPlatform == nil {
-			return fmt.Errorf("required platform %s:%s not installed", referencedPackageName, architecture)
-		} else {
-			referencedPlatformRelease = pm.GetInstalledPlatformRelease(referencedPlatform)
-		}
-	}
-
-	// Build configuration for upload
-	uploadProperties := properties.NewMap()
-	if referencedPlatformRelease != nil {
-		uploadProperties.Merge(referencedPlatformRelease.Properties)
-	}
-	uploadProperties.Merge(board.PlatformRelease.Properties)
-	uploadProperties.Merge(board.PlatformRelease.RuntimeProperties())
-	uploadProperties.Merge(boardProperties)
-
-	uploadToolProperties := uploadProperties.SubTree("tools." + uploadToolPattern)
-	uploadProperties.Merge(uploadToolProperties)
-
-	if requiredTools, err := pm.FindToolsRequiredForBoard(board); err == nil {
-		for _, requiredTool := range requiredTools {
-			logrus.WithField("tool", requiredTool).Info("Tool required for upload")
-			uploadProperties.Merge(requiredTool.RuntimeProperties())
-		}
-	}
-
-	// Set properties for verbose upload
-	Verbose := req.GetVerbose()
-	if Verbose {
-		if v, ok := uploadProperties.GetOk("upload.params.verbose"); ok {
-			uploadProperties.Set("upload.verbose", v)
-		}
-	} else {
-		if v, ok := uploadProperties.GetOk("upload.params.quiet"); ok {
-			uploadProperties.Set("upload.verbose", v)
-		}
-	}
-
-	// Set path to compiled binary
-	// Make the filename without the FQBN configs part
-	fqbn.Configs = properties.NewMap()
-	fqbnSuffix := strings.Replace(fqbn.String(), ":", ".", -1)
-
-	var importPath *paths.Path
-	var importFile string
-	if req.GetImportFile() == "" {
-		importPath = sketch.FullPath
-		importFile = sketch.Name + "." + fqbnSuffix
-	} else {
-		importPath = paths.New(req.GetImportFile()).Parent()
-		importFile = paths.New(req.GetImportFile()).Base()
-	}
-
-	outputTmpFile, ok := uploadProperties.GetOk("recipe.output.tmp_file")
-	outputTmpFile = uploadProperties.ExpandPropsInString(outputTmpFile)
-	if !ok {
-		return fmt.Errorf("property 'recipe.output.tmp_file' not defined")
-	}
-	ext := filepath.Ext(outputTmpFile)
-	if strings.HasSuffix(importFile, ext) {
-		importFile = importFile[:len(importFile)-len(ext)]
-	}
-
-	uploadProperties.SetPath("build.path", importPath)
-	uploadProperties.Set("build.project_name", importFile)
-	uploadFile := importPath.Join(importFile + ext)
-	if _, err := uploadFile.Stat(); err != nil {
-		if os.IsNotExist(err) {
-			return fmt.Errorf("compiled sketch %s not found", uploadFile.String())
-		}
-		return fmt.Errorf("cannot open sketch: %s", err)
-	}
-
-	// Build recipe for upload
-	recipe := uploadProperties.Get("upload.pattern")
-	cmdLine := uploadProperties.ExpandPropsInString(recipe)
-	cmdArgs, err := properties.SplitQuotedString(cmdLine, `"'`, false)
-	if err != nil {
-		return fmt.Errorf("invalid recipe '%s': %s", recipe, err)
-	}
-
+func Debug(ctx context.Context, req *dbg.DebugReq, inStream dbg.Debug_StreamingOpenServer, out io.Writer) (*dbg.StreamingOpenResp, error) {
 	cmdArgs := []string{"gdb"}
 	// Run Tool
 	cmd, err := executils.Command(cmdArgs)
@@ -177,5 +33,72 @@ func Debug(ctx context.Context, req *dbg.DebugReq, in func(data []bytes), out fu
 		return nil, fmt.Errorf("cannot execute upload tool: %s", err)
 	}
 
-	return cmd, nil
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println("%v\n", err)
+		return &dbg.StreamingOpenResp{}, nil // TODO: send error in response
+	}
+	defer in.Close()
+
+	cmd.Stdout = out
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("%v\n", err)
+		return &dbg.StreamingOpenResp{}, nil // TODO: send error in response
+	}
+
+	// we'll use these channels to communicate with the goroutines
+	// handling the stream and the target respectively
+	streamClosed := make(chan error)
+	targetClosed := make(chan error)
+	defer close(streamClosed)
+	defer close(targetClosed)
+
+	// now we can read the other commands and re-route to the Debug Client...
+	go func() {
+		for {
+			command, err := inStream.Recv()
+			if err == io.EOF {
+				// stream was closed
+				streamClosed <- nil
+				break
+			}
+
+			if err != nil {
+				// error reading from stream
+				streamClosed <- err
+				break
+			}
+
+			if _, err := in.Write(command.GetData()); err != nil {
+				// error writing to target
+				targetClosed <- err
+				break
+			}
+		}
+	}()
+
+	// let goroutines route messages from/to the Debug
+	// until either the client closes the stream or the
+	// Debug target is closed
+	for {
+		select {
+		case <-ctx.Done():
+			cmd.Process.Kill()
+			cmd.Wait()
+		case err := <-streamClosed:
+			fmt.Println("streamClosed")
+			cmd.Process.Kill()
+			cmd.Wait()
+			return &dbg.StreamingOpenResp{}, err // TODO: send error in response
+		case err := <-targetClosed:
+			fmt.Println("targetClosed")
+			cmd.Process.Kill()
+			cmd.Wait()
+			return &dbg.StreamingOpenResp{}, err // TODO: send error in response
+		}
+	}
+
+	return &dbg.StreamingOpenResp{}, nil
 }
