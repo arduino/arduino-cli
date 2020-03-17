285502e94b8339f3d4bb7507d6209c965ff84dac
Cristian Maglie <c.maglie@arduino.cc>
2020-03-17 12:54:24 +0100

    legacy: updated ErrrofWithLogger to use github.com/pkg/errors

--- legacy/builder/i18n/errors.go
+++ legacy/builder/i18n/errors.go
@@ -4,13 +4,13 @@ import (
 	"os"
 
 	"github.com/arduino/arduino-cli/legacy/builder/constants"
-	"github.com/go-errors/errors"
+	"github.com/pkg/errors"
 )
 
-func ErrorfWithLogger(logger Logger, format string, a ...interface{}) *errors.Error {
+func ErrorfWithLogger(logger Logger, format string, a ...interface{}) error {
 	if logger.Name() == "machine" {
 		logger.Fprintln(os.Stderr, constants.LOG_LEVEL_ERROR, format, a...)
-		return errors.Errorf("")
+		return errors.New("")
 	}
-	return errors.Errorf(Format(format, a...))
+	return errors.New(Format(format, a...))
 }
