d1103c29c22cbe9916082021c54a2644d4a06da7
Henrique <hdiniz@outlook.com>
2020-04-24 15:43:46 -0300

    add po merge function

---
+++ i18n/cmd/po/merge.go
@@ -0,0 +1,26 @@
+package po
+
+func Merge(source MessageCatalog, destination MessageCatalog) MessageCatalog {
+	catalog := MessageCatalog{}
+	for _, k := range source.SortedKeys() {
+		if k == "" {
+			sourceMessage := source.Messages[k]
+			destinationMessage := destination.Messages[k]
+
+			if destinationMessage != nil {
+				catalog.AddMessage(k, *destinationMessage)
+			} else {
+				catalog.AddMessage(k, *sourceMessage)
+			}
+
+			continue
+		}
+
+		if destination.Messages[k] != nil {
+			catalog.Add(k, destination.Messages[k].Value, source.Messages[k].Comments)
+		} else {
+			catalog.Add(k, "", source.Messages[k].Comments)
+		}
+	}
+	return catalog
+}
