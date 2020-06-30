b0b6277fcfb89a85586e2634a544fc291aef7630
Cristian Maglie <c.maglie@arduino.cc>
2020-06-30 22:03:12 +0200

    Updated go-properties-orderedmap and fixed typo
    https://github.com/arduino/go-properties-orderedmap/commit/7e1498ef4c30c1569a3557c5f95131a411a06740#commitcomment-40162715

--- commands/upload/upload.go
+++ commands/upload/upload.go
@@ -355,7 +355,7 @@ func runTool(recipeID string, props *properties.Map, outStream, errStream io.Wri
 	if strings.TrimSpace(recipe) == "" {
 		return nil // Nothing to run
 	}
-	if props.IsProertyMissingInExpandPropsInString("serial.port", recipe) {
+	if props.IsPropertyMissingInExpandPropsInString("serial.port", recipe) {
 		return fmt.Errorf("no upload port provided")
 	}
 	cmdLine := props.ExpandPropsInString(recipe)
