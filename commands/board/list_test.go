ed2fbe894a05b59e119bc4af9d70343c7d2a2777
Silvano Cerza <silvanocerza@gmail.com>
2020-10-13 17:06:49 +0200

    Remove useless function

--- commands/board/list_test.go
+++ commands/board/list_test.go
@@ -31,10 +31,6 @@ func init() {
 	configuration.Settings = configuration.Init("")
 }
 
-func reset() {
-	configuration.Settings = configuration.Init("")
-}
-
 func TestGetByVidPid(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, `
