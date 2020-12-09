7081b38e357d24190103a15f21d05692a58b3543
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2020-12-09 12:08:37 +0100

    Fix debug command not finding sketch build folder (#1104)

---
+++ test/test_debug.py
@@ -0,0 +1,39 @@
+# This file is part of arduino-cli.
+#
+# Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+#
+# This software is released under the GNU General Public License version 3,
+# which covers the main part of arduino-cli.
+# The terms of this license can be found at:
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# You can be released from the requirements of the above licenses by purchasing
+# a commercial license. Buying such a license is mandatory if you want to modify or
+# otherwise use the software for commercial activities involving the Arduino
+# software without disclosing the source code of your own applications. To purchase
+# a commercial license, send an email to license@arduino.cc.
+
+
+from pathlib import Path
+
+
+def test_debugger_starts(run_command, data_dir):
+    # Init the environment explicitly
+    assert run_command("core update-index")
+
+    # Install cores
+    assert run_command("core install arduino:samd")
+
+    # Create sketch for testing
+    sketch_name = "DebuggerStartTest"
+    sketch_path = Path(data_dir, sketch_name)
+    fqbn = "arduino:samd:mkr1000"
+
+    assert run_command(f"sketch new {sketch_path}")
+
+    # Build sketch
+    assert run_command(f"compile -b {fqbn} {sketch_path}")
+
+    programmer = "atmel_ice"
+    # Starts debugger
+    assert run_command(f"debug -b {fqbn} -P {programmer} {sketch_path} --info")
