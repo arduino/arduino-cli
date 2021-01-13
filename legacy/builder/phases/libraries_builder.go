2cbd7535b83503797f20f2af018125748b6bf8c4
Martino Facchin <m.facchin@arduino.cc>
2021-01-13 15:10:48 +0100

    Precompiled libraries: only select a folder if it contains files

--- legacy/builder/phases/libraries_builder.go
+++ legacy/builder/phases/libraries_builder.go
@@ -53,6 +53,18 @@ func (s *LibrariesBuilder) Run(ctx *types.Context) error {
 	return nil
 }
 
+func containsFile(folder *paths.Path) bool {
+	res, _ := folder.ReadDir()
+	hasfiles := false
+	for _, el := range res {
+		if !el.IsDir() {
+			hasfiles = true
+			break
+		}
+	}
+	return hasfiles
+}
+
 func findExpectedPrecompiledLibFolder(ctx *types.Context, library *libraries.Library) *paths.Path {
 	mcu := ctx.BuildProperties.Get(constants.BUILD_PROPERTIES_BUILD_MCU)
 	// Add fpu specifications if they exist
@@ -86,7 +98,7 @@ func findExpectedPrecompiledLibFolder(ctx *types.Context, library *libraries.Lib
 	if len(fpuSpecs) > 0 {
 		fpuSpecs = strings.TrimRight(fpuSpecs, "-")
 		fullPrecompDir := library.SourceDir.Join(mcu).Join(fpuSpecs)
-		if fullPrecompDir.Exist() {
+		if fullPrecompDir.Exist() && containsFile(fullPrecompDir) {
 			logger.Fprintln(os.Stdout, constants.LOG_LEVEL_INFO, "Using precompiled library in {0}", fullPrecompDir)
 			return fullPrecompDir
 		}
@@ -94,7 +106,7 @@ func findExpectedPrecompiledLibFolder(ctx *types.Context, library *libraries.Lib
 	}
 
 	precompDir := library.SourceDir.Join(mcu)
-	if precompDir.Exist() {
+	if precompDir.Exist() && containsFile(precompDir) {
 		logger.Fprintln(os.Stdout, constants.LOG_LEVEL_INFO, "Using precompiled library in {0}", precompDir)
 		return precompDir
 	}
