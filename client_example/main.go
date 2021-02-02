85b824c9e3b16a06d38599f5c7c40f117aeb5b29
Silvano Cerza <silvanocerza@gmail.com>
2021-02-02 16:21:51 +0100

    [skip changelog] Add clearer example to client_example

--- client_example/main.go
+++ client_example/main.go
@@ -84,10 +84,19 @@ func main() {
 
 	// Merge applies multiple settings values at once.
 	log.Println("calling Merge()")
-	callMerge(settingsClient)
+	callMerge(settingsClient, `{"foo": {"value": "bar"}, "daemon":{"port":"422"}, "board_manager": {"additional_urls":["https://example.com"]}}`)
 
 	log.Println("calling GetAll()")
 	callGetAll(settingsClient)
+
+	log.Println("calling Merge()")
+	callMerge(settingsClient, `{"foo": {} }`)
+
+	log.Println("calling GetAll()")
+	callGetAll(settingsClient)
+
+	log.Println("calling Merge()")
+	callMerge(settingsClient, `{"foo": "bar" }`)
 
 	// Get the value of the foo key.
 	log.Println("calling GetValue(foo)")
@@ -265,11 +274,10 @@ func callUnsetProxy(client settings.SettingsClient) {
 	}
 }
 
-func callMerge(client settings.SettingsClient) {
-	bulkSettings := `{"foo": "bar", "daemon":{"port":"422"}, "board_manager": {"additional_urls":["https://example.com"]}}`
+func callMerge(client settings.SettingsClient, jsonData string) {
 	_, err := client.Merge(context.Background(),
 		&settings.RawData{
-			JsonData: bulkSettings,
+			JsonData: jsonData,
 		})
 
 	if err != nil {
