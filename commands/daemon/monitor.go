4e18db213718e3654d88d845852af2b55a691e48
Cristian Maglie <c.maglie@arduino.cc>
2021-03-11 15:22:10 +0100

    Added monitor rate limiting

--- commands/daemon/monitor.go
+++ commands/daemon/monitor.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"sync/atomic"
 
 	"github.com/arduino/arduino-cli/arduino/monitors"
 	rpc "github.com/arduino/arduino-cli/rpc/monitor"
@@ -79,6 +80,17 @@ func (s *MonitorService) StreamingOpen(stream rpc.Monitor_StreamingOpenServer) e
 	streamClosed := make(chan error)
 	targetClosed := make(chan error)
 
+	// set rate limiting window
+	bufferSize := int(config.GetRecvRateLimitBuffer())
+	rateLimitEnabled := (bufferSize > 0)
+	if !rateLimitEnabled {
+		bufferSize = 1024
+	}
+	buffer := make([]byte, bufferSize)
+	bufferUsed := 0
+
+	var writeSlots int32
+
 	// now we can read the other messages and re-route to the monitor...
 	go func() {
 		for {
@@ -95,6 +107,11 @@ func (s *MonitorService) StreamingOpen(stream rpc.Monitor_StreamingOpenServer) e
 				break
 			}
 
+			if rateLimitEnabled {
+				// Increase rate limiter write slots
+				atomic.AddInt32(&writeSlots, msg.GetRecvAcknowledge())
+			}
+
 			if _, err := mon.Write(msg.GetData()); err != nil {
 				// error writing to target
 				targetClosed <- err
@@ -105,27 +122,59 @@ func (s *MonitorService) StreamingOpen(stream rpc.Monitor_StreamingOpenServer) e
 
 	// ...and read from the monitor and forward to the output stream
 	go func() {
-		buf := make([]byte, 8)
+		dropBuffer := make([]byte, 10240)
+		dropped := 0
 		for {
-			n, err := mon.Read(buf)
-			if err != nil {
-				// error reading from target
-				targetClosed <- err
-				break
+			if bufferUsed < bufferSize {
+				if n, err := mon.Read(buffer[bufferUsed:]); err != nil {
+					// error reading from target
+					targetClosed <- err
+					break
+				} else if n == 0 {
+					// target was closed
+					targetClosed <- nil
+					break
+				} else {
+					bufferUsed += n
+				}
+			} else {
+				// FIXME: a very rare condition but still...
+				// we may be waiting here while, in the meantime, a transmit slot is
+				// freed: in this case the (filled) buffer will stay in the server
+				// until the following Read exits (-> the next char arrives from the
+				// monitor).
+
+				if n, err := mon.Read(dropBuffer); err != nil {
+					// error reading from target
+					targetClosed <- err
+					break
+				} else if n == 0 {
+					// target was closed
+					targetClosed <- nil
+					break
+				} else {
+					dropped += n
+				}
 			}
 
-			if n == 0 {
-				// target was closed
-				targetClosed <- nil
-				break
-			}
+			slots := atomic.LoadInt32(&writeSlots)
+			if !rateLimitEnabled || slots > 0 {
+				if err = stream.Send(&rpc.StreamingOpenResp{
+					Data:    buffer[:bufferUsed],
+					Dropped: int32(dropped),
+				}); err != nil {
+					// error sending to stream
+					streamClosed <- err
+					break
+				}
+				bufferUsed = 0
+				dropped = 0
 
-			if err = stream.Send(&rpc.StreamingOpenResp{
-				Data: buf[:n],
-			}); err != nil {
-				// error sending to stream
-				streamClosed <- err
-				break
+				// Rate limit, filling all the available window
+				if rateLimitEnabled {
+					slots = atomic.AddInt32(&writeSlots, -1)
+					//fmt.Println("FREE SLOTS:", slots)
+				}
 			}
 		}
 	}()
