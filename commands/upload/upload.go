0c9bcb235c77be49a12ecd00855ccede6c6e8915
irieda <irieda@gmail.com>
2021-06-07 18:40:07 +0900

    add runtime.os property

--- commands/upload/upload.go
+++ commands/upload/upload.go
@@ -205,6 +205,7 @@ func runProgramAction(pm *packagemanager.PackageManager,
 	if uploadToolPlatform != nil {
 		uploadProperties.Merge(uploadToolPlatform.Properties)
 	}
+	uploadProperties.Set("runtime.os", properties.GetOSSuffix())
 	uploadProperties.Merge(boardPlatform.Properties)
 	uploadProperties.Merge(boardPlatform.RuntimeProperties())
 	uploadProperties.Merge(boardProperties)
