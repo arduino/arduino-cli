95b69faa73ce753895a61d8d4ee12c24f6995eee
Silvano Cerza <silvanocerza@gmail.com>
2021-07-27 14:38:20 +0200

    Add more upload mock integration tests

--- test/test_upload_mock.py
+++ test/test_upload_mock.py
@@ -28,6 +28,7 @@ def generate_build_dir(sketch_path):
 
 testdata = [
     (
+        "",
         "arduino:avr:uno",
         "arduino:avr@1.8.3",
         "/dev/ttyACM0",
@@ -36,6 +37,7 @@ testdata = [
         + '-v -V -patmega328p -carduino "-P{upload_port}" -b115200 -D "-Uflash:w:{build_dir}/{sketch_name}.ino.hex:i"',
     ),
     (
+        "",
         "arduino:avr:leonardo",
         "arduino:avr@1.8.3",
         "/dev/ttyACM999",
@@ -43,16 +45,63 @@ testdata = [
         + '"-C{data_dir}/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf" '
         + '-v -V -patmega32u4 -cavr109 "-P{upload_port}0" -b57600 -D "-Uflash:w:{build_dir}/{sketch_name}.ino.hex:i"',
     ),
+    (
+        "https://adafruit.github.io/arduino-board-index/package_adafruit_index.json",
+        "adafruit:avr:flora8",
+        "adafruit:avr@1.4.13",
+        "/dev/ttyACM0",
+        '"{data_dir}/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude" '
+        + '"-C{data_dir}/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf" '
+        + '-v -patmega32u4 -cavr109 -P{upload_port} -b57600 -D "-Uflash:w:{build_dir}/{sketch_name}.ino.hex:i"',
+    ),
+    (
+        "https://adafruit.github.io/arduino-board-index/package_adafruit_index.json",
+        "adafruit:avr:flora8",
+        "adafruit:avr@1.4.13",
+        "/dev/ttyACM999",
+        '"{data_dir}/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude" '
+        + '"-C{data_dir}/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf" '
+        + '-v -patmega32u4 -cavr109 -P{upload_port}0 -b57600 -D "-Uflash:w:{build_dir}/{sketch_name}.ino.hex:i"',
+    ),
+    (
+        "https://dl.espressif.com/dl/package_esp32_index.json",
+        "esp32:esp32:esp32thing",
+        "esp32:esp32@1.0.6",
+        "/dev/ttyACM0",
+        'python "{data_dir}/packages/esp32/tools/esptool_py/3.0.0/esptool.py" '
+        + '--chip esp32 --port "{upload_port}" --baud 921600  --before default_reset '
+        + "--after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 "
+        + '"{data_dir}/packages/esp32/hardware/esp32/1.0.6/tools/partitions/boot_app0.bin" 0x1000 '
+        + '"{data_dir}/packages/esp32/hardware/esp32/1.0.6/tools/sdk/bin/bootloader_dio_80m.bin" 0x10000 '
+        + '"{build_dir}/{sketch_name}.ino.bin" 0x8000 "{build_dir}/{sketch_name}.ino.partitions.bin"',
+    ),
+    (
+        "http://arduino.esp8266.com/stable/package_esp8266com_index.json",
+        "esp8266:esp8266:generic",
+        "esp8266:esp8266@3.0.1",
+        "/dev/ttyACM0",
+        '"{data_dir}/packages/esp8266/tools/python3/3.7.2-post1/python3" '
+        + '"{data_dir}/packages/esp8266/hardware/esp8266/3.0.1/tools/upload.py" '
+        + '--chip esp8266 --port "{upload_port}" --baud "115200" ""  '
+        + "--before default_reset --after hard_reset write_flash 0x0 "
+        + '"{build_dir}/{sketch_name}.ino.bin"',
+    ),
 ]
 
 
-@pytest.mark.parametrize("fqbn, core, upload_port, expected_output", testdata)
-def test_upload_sketch(run_command, session_data_dir, downloads_dir, fqbn, core, upload_port, expected_output):
+@pytest.mark.parametrize("index, fqbn, core, upload_port, expected_output", testdata)
+def test_upload_sketch(run_command, session_data_dir, downloads_dir, index, fqbn, core, upload_port, expected_output):
     env = {
         "ARDUINO_DATA_DIR": session_data_dir,
         "ARDUINO_DOWNLOADS_DIR": downloads_dir,
         "ARDUINO_SKETCHBOOK_DIR": session_data_dir,
     }
+
+    if index:
+        assert run_command("config init --overwrite", custom_env=env)
+        assert run_command(f"config add board_manager.additional_urls {index}", custom_env=env)
+        assert run_command("update", custom_env=env)
+
     assert run_command(f"core install {core}", custom_env=env)
 
     # Create a sketch
