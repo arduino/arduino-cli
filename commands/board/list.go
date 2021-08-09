f07775a2131fe602021731691666dcd5dd6e3027
Silvano Cerza <silvanocerza@gmail.com>
2021-08-09 17:45:06 +0200

    Implemented board watch with discoveries

--- commands/board/list.go
+++ commands/board/list.go
@@ -227,40 +227,49 @@ func List(instanceID int32) (r []*rpc.DetectedPort, e error) {
 // The discovery process can be interrupted by sending a message to the interrupt channel.
 func Watch(instanceID int32, interrupt <-chan bool) (<-chan *rpc.BoardListWatchResponse, error) {
 	pm := commands.GetPackageManager(instanceID)
-	eventsChan, err := commands.WatchListBoards(pm)
-	if err != nil {
+
+	if err := pm.DiscoveryManager().RunAll(); err != nil {
 		return nil, err
 	}
 
+	eventsChan, errs := pm.DiscoveryManager().StartSyncAll()
+
 	outChan := make(chan *rpc.BoardListWatchResponse)
+
 	go func() {
+		for _, err := range errs {
+			outChan <- &rpc.BoardListWatchResponse{
+				EventType: "error",
+				Error:     err.Error(),
+			}
+		}
 		for {
 			select {
 			case event := <-eventsChan:
-				boards := []*rpc.BoardListItem{}
-				boardsError := ""
-				if event.Type == "add" {
-					boards, err = identify(pm, event.Port)
-					if err != nil {
-						boardsError = err.Error()
-					}
-				}
-
 				serialNumber := ""
 				if props := event.Port.Properties; props != nil {
 					serialNumber = props.Get("serialNumber")
 				}
 
+				port := &rpc.DetectedPort{
+					Address:       event.Port.Address,
+					Protocol:      event.Port.Protocol,
+					ProtocolLabel: event.Port.ProtocolLabel,
+					SerialNumber:  serialNumber,
+				}
+
+				boardsError := ""
+				if event.Type == "add" {
+					boards, err := identify(pm, event.Port)
+					if err != nil {
+						boardsError = err.Error()
+					}
+					port.Boards = boards
+				}
 				outChan <- &rpc.BoardListWatchResponse{
 					EventType: event.Type,
-					Port: &rpc.DetectedPort{
-						Address:       event.Port.Address,
-						Protocol:      event.Port.Protocol,
-						ProtocolLabel: event.Port.ProtocolLabel,
-						Boards:        boards,
-						SerialNumber:  serialNumber,
-					},
-					Error: boardsError,
+					Port:      port,
+					Error:     boardsError,
 				}
 			case <-interrupt:
 				return
