64434e5925de22c65fb023cf87d2561a70370957
Cristian Maglie <c.maglie@arduino.cc>
2021-10-05 18:56:01 +0200

    Implementation of gRPC Monitor API (#1496)

--- commands/daemon/term_example/main.go
+++ commands/daemon/term_example/main.go
@@ -17,20 +17,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
-	monitor "github.com/arduino/arduino-cli/rpc/cc/arduino/cli/monitor/v1"
+	"github.com/arduino/arduino-cli/rpc/cc/arduino/cli/commands/v1"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
-var (
-	dataDir string
-)
-
-// This program exercise monitor rate limiting functionality.
+// This program exercise CLI monitor functionality.
 
 func main() {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
@@ -39,58 +36,90 @@ func main() {
 	}
 	defer conn.Close()
 
-	// Open a monitor instance
-	mon := monitor.NewMonitorServiceClient(conn)
-	stream, err := mon.StreamingOpen(context.Background())
-	if err != nil {
-		log.Fatal("Error opening stream:", err)
+	// Create and initialize a CLI instance
+	cli := commands.NewArduinoCoreServiceClient(conn)
+
+	var instance *commands.Instance
+	if resp, err := cli.Create(context.Background(), &commands.CreateRequest{}); err != nil {
+		log.Fatal("Create:", err)
+	} else {
+		instance = resp.Instance
 	}
 
-	additionalConf, err := structpb.NewStruct(
-		map[string]interface{}{"OutputRate": float64(1000000)},
-	)
-	if err != nil {
-		log.Fatal("Error creating config:", err)
-	}
-
-	if err := stream.Send(&monitor.StreamingOpenRequest{
-		Content: &monitor.StreamingOpenRequest_Config{
-			Config: &monitor.MonitorConfig{
-				Type:                monitor.MonitorConfig_TARGET_TYPE_NULL,
-				AdditionalConfig:    additionalConf,
-				RecvRateLimitBuffer: 1024,
-			},
-		},
-	}); err != nil {
-		log.Fatal("Error opening stream:", err)
-	}
-
-	if err := stream.Send(&monitor.StreamingOpenRequest{
-		Content: &monitor.StreamingOpenRequest_RecvAcknowledge{
-			RecvAcknowledge: 5,
-		},
-	}); err != nil {
-		log.Fatal("Error replenishing recv window:", err)
-	}
-
-	for {
-		r, err := stream.Recv()
-		if err != nil {
-			log.Fatal("Error receiving from server:", err)
+	if respStream, err := cli.Init(context.Background(), &commands.InitRequest{Instance: instance}); err != nil {
+		log.Fatal("Init:", err)
+	} else {
+		for {
+			resp, err := respStream.Recv()
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			if err != nil {
+				log.Fatal("Init:", err)
+			}
+			fmt.Println(resp)
 		}
-		if l := len(r.Data); l > 0 {
-			fmt.Printf("RECV %d bytes\n", l)
+	}
+
+	// List boards and take the first available port
+	var port *commands.Port
+	if resp, err := cli.BoardList(context.Background(), &commands.BoardListRequest{Instance: instance}); err != nil {
+		log.Fatal("BoardList:", err)
+	} else {
+		ports := resp.GetPorts()
+		if len(ports) == 0 {
+			log.Fatal("No port to connect!")
 		}
-		if r.Dropped > 0 {
-			fmt.Printf("DROPPED %d bytes!!!\n", r.Dropped)
-		}
-		if err := stream.Send(&monitor.StreamingOpenRequest{
-			Content: &monitor.StreamingOpenRequest_RecvAcknowledge{
-				RecvAcknowledge: 1,
-			},
+		port = ports[0].Port
+	}
+	fmt.Println("Detected port:", port.Label, port.ProtocolLabel)
+
+	// Connect to the port monitor
+	fmt.Println("Connecting to monitor")
+	ctx, cancel := context.WithCancel(context.Background())
+	if respStream, err := cli.Monitor(ctx); err != nil {
+		log.Fatal("Monitor:", err)
+	} else {
+		if err := respStream.Send(&commands.MonitorRequest{
+			Instance: instance,
+			Port:     port,
 		}); err != nil {
-			log.Fatal("Error replenishing recv window:", err)
+			log.Fatal("Monitor send-config:", err)
 		}
-		time.Sleep(5 * time.Millisecond)
+		time.Sleep(1 * time.Second)
+
+		go func() {
+			for {
+				if resp, err := respStream.Recv(); err != nil {
+					fmt.Println("     RECV:", err)
+					break
+				} else {
+					fmt.Println("     RECV:", resp)
+				}
+			}
+		}()
+
+		hello := &commands.MonitorRequest{
+			TxData: []byte("HELLO!"),
+		}
+		fmt.Println("Send:", hello)
+		if err := respStream.Send(hello); err != nil {
+			log.Fatal("Monitor send HELLO:", err)
+		}
+
+		fmt.Println("Send:", hello)
+		if err := respStream.Send(hello); err != nil {
+			log.Fatal("Monitor send HELLO:", err)
+		}
+
+		time.Sleep(5 * time.Second)
+
+		fmt.Println("Closing Monitor")
+		if err := respStream.CloseSend(); err != nil {
+			log.Fatal("Monitor close send:", err)
+		}
+		time.Sleep(5 * time.Second)
 	}
+	cancel()
+	time.Sleep(5 * time.Second)
 }
