b6ed62fa0d3a5bdd55f7edcc44ebdaf9f8397f8f
Umberto Baldi <u.baldi@arduino.cc>
2021-10-05 18:21:26 +0200

    the previous implementation was working only with a board connected to the pc RPC was not exposing that functionality

--- cli/arguments/completion.go
+++ cli/arguments/completion.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/arduino/arduino-cli/cli/instance"
+	"github.com/arduino/arduino-cli/commands"
 	"github.com/arduino/arduino-cli/commands/board"
 	rpc "github.com/arduino/arduino-cli/rpc/cc/arduino/cli/commands/v1"
 )
@@ -28,16 +29,28 @@ func GetInstalledBoards(toComplete string) []string {
 }
 
 // GetInstalledProtocols is an helper function usefull to autocomplete.
-// It returns a list of protocols available
+// It returns a list of protocols available based on the installed boards
 func GetInstalledProtocols(toComplete string) []string {
 	inst := instance.CreateAndInit() // TODO optimize this: it does not make sense to create an instance everytime
+	pm := commands.GetPackageManager(inst.Id)
+	boards := pm.InstalledBoards()
 
-	detectedBoards, _ := board.List(&rpc.BoardListRequest{
-		Instance: inst,
-	})
-	var res []string
-	for _, i := range detectedBoards {
-		res = append(res, i.Port.Protocol)
+	// use this strange map because it should be more optimized
+	// we use only the key and not the value because we do not need it
+	intalledProtocolsMap := make(map[string]struct{})
+	for _, board := range boards {
+		// we filter and elaborate a bit the informations present in Properties
+		for _, protocol := range board.Properties.SubTree("upload.tool").FirstLevelKeys() {
+			if protocol != "default" { // remove this value since it's the default one
+				intalledProtocolsMap[protocol] = struct{}{}
+			}
+		}
+	}
+	res := make([]string, len(intalledProtocolsMap))
+	i := 0
+	for k := range intalledProtocolsMap {
+		res[i] = k
+		i++
 	}
 	return res
 }
