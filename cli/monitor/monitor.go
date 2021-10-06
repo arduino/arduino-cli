fd14435e7937753c6f132f7aac626c9107b152fe
Cristian Maglie <c.maglie@arduino.cc>
2021-10-06 14:16:47 +0200

    Add `--quiet` and `--config` flags (#1497)
    * Added silent mode for monitor
    
    * Added --config flag to monitor command
    
    * fix i18n
    
    * Applied review suggestions
    
    * rename --silent to --quiet
    * removed useless 'settings' variable
    * translation string for "Monitor port settings" in CLI mode

--- cli/monitor/monitor.go
+++ cli/monitor/monitor.go
@@ -18,6 +18,7 @@ package monitor
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"sort"
@@ -28,6 +29,7 @@ import (
 	"github.com/arduino/arduino-cli/cli/feedback"
 	"github.com/arduino/arduino-cli/cli/instance"
 	"github.com/arduino/arduino-cli/commands/monitor"
+	"github.com/arduino/arduino-cli/configuration"
 	"github.com/arduino/arduino-cli/i18n"
 	rpc "github.com/arduino/arduino-cli/rpc/cc/arduino/cli/commands/v1"
 	"github.com/arduino/arduino-cli/table"
@@ -39,6 +41,8 @@ var tr = i18n.Tr
 
 var portArgs arguments.Port
 var describe bool
+var configs []string
+var quiet bool
 
 // NewCommand created a new `monitor` command
 func NewCommand() *cobra.Command {
@@ -53,12 +57,18 @@ func NewCommand() *cobra.Command {
 	}
 	portArgs.AddToCommand(cmd)
 	cmd.Flags().BoolVar(&describe, "describe", false, tr("Show all the settings of the communication port."))
+	cmd.Flags().StringSliceVarP(&configs, "config", "c", []string{}, tr("Configuration of the port."))
+	cmd.Flags().BoolVarP(&quiet, "quiet", "q", false, tr("Run in silent mode, show only monitor input and output."))
 	cmd.MarkFlagRequired("port")
 	return cmd
 }
 
 func runMonitorCmd(cmd *cobra.Command, args []string) {
 	instance := instance.CreateAndInit()
+
+	if !configuration.HasConsole {
+		quiet = true
+	}
 
 	port, err := portArgs.GetPort(instance, nil)
 	if err != nil {
@@ -87,10 +97,63 @@ func runMonitorCmd(cmd *cobra.Command, args []string) {
 	}
 	defer tty.Close()
 
+	configuration := &rpc.MonitorPortConfiguration{}
+	if len(configs) > 0 {
+		resp, err := monitor.EnumerateMonitorPortSettings(context.Background(), &rpc.EnumerateMonitorPortSettingsRequest{
+			Instance: instance,
+			Port:     port.ToRPC(),
+			Fqbn:     "",
+		})
+		if err != nil {
+			feedback.Error(err)
+			os.Exit(errorcodes.ErrGeneric)
+		}
+		for _, config := range configs {
+			split := strings.SplitN(config, "=", 2)
+			k := ""
+			v := config
+			if len(split) == 2 {
+				k = split[0]
+				v = split[1]
+			}
+
+			var setting *rpc.MonitorPortSettingDescriptor
+			for _, s := range resp.GetSettings() {
+				if k == "" {
+					if contains(s.EnumValues, v) {
+						setting = s
+						break
+					}
+				} else {
+					if strings.EqualFold(s.SettingId, k) {
+						if !contains(s.EnumValues, v) {
+							feedback.Error(tr("invalid port configuration value for %s: %s", k, v))
+							os.Exit(errorcodes.ErrBadArgument)
+						}
+						setting = s
+						break
+					}
+				}
+			}
+			if setting == nil {
+				feedback.Error(tr("invalid port configuration: %s", config))
+				os.Exit(errorcodes.ErrBadArgument)
+			}
+			configuration.Settings = append(configuration.Settings, &rpc.MonitorPortSetting{
+				SettingId: setting.SettingId,
+				Value:     v,
+			})
+			if !quiet {
+				feedback.Print(tr("Monitor port settings:"))
+				feedback.Print(fmt.Sprintf("%s=%s", setting.SettingId, v))
+			}
+		}
+	}
 	portProxy, _, err := monitor.Monitor(context.Background(), &rpc.MonitorRequest{
-		Instance: instance,
-		Port:     port.ToRPC(),
-		Fqbn:     "",
+		Instance:          instance,
+		Port:              port.ToRPC(),
+		Fqbn:              "",
+		PortConfiguration: configuration,
 	})
 	if err != nil {
 		feedback.Error(err)
@@ -114,7 +177,9 @@ func runMonitorCmd(cmd *cobra.Command, args []string) {
 		cancel()
 	}()
 
-	feedback.Print(tr("Connected to %s! Press CTRL-C to exit.", port.String()))
+	if !quiet {
+		feedback.Print(tr("Connected to %s! Press CTRL-C to exit.", port.String()))
+	}
 
 	// Wait for port closed
 	<-ctx.Done()
@@ -140,4 +205,13 @@ func (r *detailsResult) String() string {
 		t.AddRow(setting.SettingId, setting.Label, table.NewCell(setting.Value, green), values)
 	}
 	return t.Render()
+}
+
+func contains(s []string, searchterm string) bool {
+	for _, item := range s {
+		if strings.EqualFold(item, searchterm) {
+			return true
+		}
+	}
+	return false
 }
