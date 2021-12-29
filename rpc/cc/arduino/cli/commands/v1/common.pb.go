4506bf9d3601d6787df3948a8008e32f7dd32430
Cristian Maglie <c.maglie@arduino.cc>
2021-12-29 17:44:07 +0100

    Report board platform and build platform on `compile` JSON output (#1608)
    * Added 'board_platform' and 'build_platform' fields in gRPC Compile command
    
    * Report board platform and build platform on compile output

--- rpc/cc/arduino/cli/commands/v1/common.pb.go
+++ rpc/cc/arduino/cli/commands/v1/common.pb.go
@@ -430,6 +430,63 @@ func (x *Platform) GetDeprecated() bool {
 	return false
 }
 
+type PlatformReference struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Platform ID (e.g., `arduino:avr`).
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	// Version of the platform.
+	Version string `protobuf:"bytes,2,opt,name=version,proto3" json:"version,omitempty"`
+}
+
+func (x *PlatformReference) Reset() {
+	*x = PlatformReference{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PlatformReference) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PlatformReference) ProtoMessage() {}
+
+func (x *PlatformReference) ProtoReflect() protoreflect.Message {
+	mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PlatformReference.ProtoReflect.Descriptor instead.
+func (*PlatformReference) Descriptor() ([]byte, []int) {
+	return file_cc_arduino_cli_commands_v1_common_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *PlatformReference) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *PlatformReference) GetVersion() string {
+	if x != nil {
+		return x.Version
+	}
+	return ""
+}
+
 type Board struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -445,7 +502,7 @@ type Board struct {
 func (x *Board) Reset() {
 	*x = Board{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[5]
+		mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[6]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -458,7 +515,7 @@ func (x *Board) String() string {
 func (*Board) ProtoMessage() {}
 
 func (x *Board) ProtoReflect() protoreflect.Message {
-	mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[5]
+	mi := &file_cc_arduino_cli_commands_v1_common_proto_msgTypes[6]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -471,7 +528,7 @@ func (x *Board) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Board.ProtoReflect.Descriptor instead.
 func (*Board) Descriptor() ([]byte, []int) {
-	return file_cc_arduino_cli_commands_v1_common_proto_rawDescGZIP(), []int{5}
+	return file_cc_arduino_cli_commands_v1_common_proto_rawDescGZIP(), []int{6}
 }
 
 func (x *Board) GetName() string {
@@ -537,15 +594,19 @@ var file_cc_arduino_cli_commands_v1_common_proto_rawDesc = []byte{
 	0x08, 0x52, 0x11, 0x6d, 0x61, 0x6e, 0x75, 0x61, 0x6c, 0x6c, 0x79, 0x49, 0x6e, 0x73, 0x74, 0x61,
 	0x6c, 0x6c, 0x65, 0x64, 0x12, 0x1e, 0x0a, 0x0a, 0x64, 0x65, 0x70, 0x72, 0x65, 0x63, 0x61, 0x74,
 	0x65, 0x64, 0x18, 0x0a, 0x20, 0x01, 0x28, 0x08, 0x52, 0x0a, 0x64, 0x65, 0x70, 0x72, 0x65, 0x63,
-	0x61, 0x74, 0x65, 0x64, 0x22, 0x2f, 0x0a, 0x05, 0x42, 0x6f, 0x61, 0x72, 0x64, 0x12, 0x12, 0x0a,
-	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d,
-	0x65, 0x12, 0x12, 0x0a, 0x04, 0x66, 0x71, 0x62, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x04, 0x66, 0x71, 0x62, 0x6e, 0x42, 0x48, 0x5a, 0x46, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x61, 0x72, 0x64, 0x75, 0x69, 0x6e, 0x6f, 0x2f, 0x61, 0x72, 0x64, 0x75,
-	0x69, 0x6e, 0x6f, 0x2d, 0x63, 0x6c, 0x69, 0x2f, 0x72, 0x70, 0x63, 0x2f, 0x63, 0x63, 0x2f, 0x61,
-	0x72, 0x64, 0x75, 0x69, 0x6e, 0x6f, 0x2f, 0x63, 0x6c, 0x69, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x61,
-	0x6e, 0x64, 0x73, 0x2f, 0x76, 0x31, 0x3b, 0x63, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x73, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x61, 0x74, 0x65, 0x64, 0x22, 0x3d, 0x0a, 0x11, 0x50, 0x6c, 0x61, 0x74, 0x66, 0x6f, 0x72, 0x6d,
+	0x52, 0x65, 0x66, 0x65, 0x72, 0x65, 0x6e, 0x63, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x65, 0x72,
+	0x73, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x76, 0x65, 0x72, 0x73,
+	0x69, 0x6f, 0x6e, 0x22, 0x2f, 0x0a, 0x05, 0x42, 0x6f, 0x61, 0x72, 0x64, 0x12, 0x12, 0x0a, 0x04,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x12, 0x12, 0x0a, 0x04, 0x66, 0x71, 0x62, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04,
+	0x66, 0x71, 0x62, 0x6e, 0x42, 0x48, 0x5a, 0x46, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
+	0x6f, 0x6d, 0x2f, 0x61, 0x72, 0x64, 0x75, 0x69, 0x6e, 0x6f, 0x2f, 0x61, 0x72, 0x64, 0x75, 0x69,
+	0x6e, 0x6f, 0x2d, 0x63, 0x6c, 0x69, 0x2f, 0x72, 0x70, 0x63, 0x2f, 0x63, 0x63, 0x2f, 0x61, 0x72,
+	0x64, 0x75, 0x69, 0x6e, 0x6f, 0x2f, 0x63, 0x6c, 0x69, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x61, 0x6e,
+	0x64, 0x73, 0x2f, 0x76, 0x31, 0x3b, 0x63, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x73, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -560,17 +621,18 @@ func file_cc_arduino_cli_commands_v1_common_proto_rawDescGZIP() []byte {
 	return file_cc_arduino_cli_commands_v1_common_proto_rawDescData
 }
 
-var file_cc_arduino_cli_commands_v1_common_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_cc_arduino_cli_commands_v1_common_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_cc_arduino_cli_commands_v1_common_proto_goTypes = []interface{}{
-	(*Instance)(nil),         // 0: cc.arduino.cli.commands.v1.Instance
-	(*DownloadProgress)(nil), // 1: cc.arduino.cli.commands.v1.DownloadProgress
-	(*TaskProgress)(nil),     // 2: cc.arduino.cli.commands.v1.TaskProgress
-	(*Programmer)(nil),       // 3: cc.arduino.cli.commands.v1.Programmer
-	(*Platform)(nil),         // 4: cc.arduino.cli.commands.v1.Platform
-	(*Board)(nil),            // 5: cc.arduino.cli.commands.v1.Board
+	(*Instance)(nil),          // 0: cc.arduino.cli.commands.v1.Instance
+	(*DownloadProgress)(nil),  // 1: cc.arduino.cli.commands.v1.DownloadProgress
+	(*TaskProgress)(nil),      // 2: cc.arduino.cli.commands.v1.TaskProgress
+	(*Programmer)(nil),        // 3: cc.arduino.cli.commands.v1.Programmer
+	(*Platform)(nil),          // 4: cc.arduino.cli.commands.v1.Platform
+	(*PlatformReference)(nil), // 5: cc.arduino.cli.commands.v1.PlatformReference
+	(*Board)(nil),             // 6: cc.arduino.cli.commands.v1.Board
 }
 var file_cc_arduino_cli_commands_v1_common_proto_depIdxs = []int32{
-	5, // 0: cc.arduino.cli.commands.v1.Platform.boards:type_name -> cc.arduino.cli.commands.v1.Board
+	6, // 0: cc.arduino.cli.commands.v1.Platform.boards:type_name -> cc.arduino.cli.commands.v1.Board
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -645,6 +707,18 @@ func file_cc_arduino_cli_commands_v1_common_proto_init() {
 			}
 		}
 		file_cc_arduino_cli_commands_v1_common_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PlatformReference); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cc_arduino_cli_commands_v1_common_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Board); i {
 			case 0:
 				return &v.state
@@ -663,7 +737,7 @@ func file_cc_arduino_cli_commands_v1_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_cc_arduino_cli_commands_v1_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
