6ac6ded40dc90ff7305bedc0f31843e4a71a98fa
Silvano Cerza <silvanocerza@gmail.com>
2022-01-26 18:28:14 +0100

    Update i18n transifex push command to use API v3

--- i18n/cmd/commands/transifex/push_transifex.go
+++ i18n/cmd/commands/transifex/push_transifex.go
@@ -17,14 +17,15 @@ package transifex
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
-	"path"
-	"path/filepath"
+	"time"
 
+	"github.com/arduino/go-paths-helper"
 	"github.com/spf13/cobra"
 )
 
@@ -35,56 +36,150 @@ var pushTransifexCommand = &cobra.Command{
 	Run:   pushCatalog,
 }
 
-func pushFile(folder, lang, url string) {
-	filename := path.Join(folder, lang+".po")
-	file, err := os.Open(filename)
+// uploadSourceFile starts an async upload of resourceFile.
+// Returns an id to monitor the upload status.
+func uploadSourceFile(resourceFile *paths.Path) string {
+	url := mainEndpoint + "resource_strings_async_uploads"
+	data, err := resourceFile.ReadFile()
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile(lang, filepath.Base(filename))
-
-	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	_, err = io.Copy(part, file)
-	writer.WriteField("file_type", "po")
+	jsonData := map[string]interface{}{
+		"data": map[string]interface{}{
+			"attributes": map[string]string{
+				"content":          base64.StdEncoding.EncodeToString(data),
+				"content_encoding": "base64",
+			},
+			"relationships": map[string]interface{}{
+				"resource": map[string]interface{}{
+					"data": map[string]string{
+						"id":   fmt.Sprintf("o:%s:p:%s:r:%s", organization, project, resource),
+						"type": "resources",
+					},
+				},
+			},
+			"type": "resource_strings_async_uploads",
+		},
+	}
 
-	req, err := http.NewRequest("PUT", url, body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
+	jsonBytes, err := json.Marshal(jsonData)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	req.SetBasicAuth("api", apiKey)
-
-	resp, err := http.DefaultClient.Do(req)
-
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		bytes.NewBuffer(jsonBytes),
+	)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	resp.Body.Close()
+
+	req.Header.Set("Content-Type", "application/vnd.api+json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var jsonRes map[string]interface{}
+	if err = json.Unmarshal(body, &jsonRes); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Started upload of resource file %s\n", resourceFile)
+	return jsonRes["data"].(map[string]interface{})["id"].(string)
+}
+
+func checkUploadStatus(uploadID string) {
+	url := mainEndpoint + "resource_strings_async_uploads/" + uploadID
+	// The upload request status must be asked from time to time, if it's
+	// still pending we try again using exponentional backoff starting from 2.5 seconds.
+	backoff := 2500 * time.Millisecond
+
+	for {
+		req, err := http.NewRequest(
+			"GET",
+			url,
+			nil,
+		)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		req.Header.Set("Content-Type", "application/vnd.api+json")
+		req.Header.Set("Authorization", "Bearer "+apiKey)
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		var body []byte
+		{
+			defer res.Body.Close()
+			body, err = io.ReadAll(res.Body)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		var jsonRes map[string]interface{}
+		if err = json.Unmarshal(body, &jsonRes); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		data := jsonRes["data"].(map[string]interface{})
+		attributes := data["attributes"].(map[string]interface{})
+		status := attributes["status"].(string)
+		switch status {
+		case "succeeded":
+			return
+		case "pending":
+			fallthrough
+		case "processing":
+			fmt.Printf("Current status: %s\n", status)
+			time.Sleep(backoff)
+			backoff = backoff * 2
+			// Request the status again
+			continue
+		case "failed":
+			errs := attributes["errors"].([]map[string]string)
+			for _, err := range errs {
+				fmt.Printf("%s: %s\n", err["code"], err["detail"])
+			}
+			os.Exit(1)
+		}
+		fmt.Println("Status request failed in an unforeseen way")
+		os.Exit(1)
+	}
+}
+
+func pushFile(resourceFile *paths.Path) {
+	uploadID := uploadSourceFile(resourceFile)
+	checkUploadStatus(uploadID)
 }
 
 func pushCatalog(cmd *cobra.Command, args []string) {
 	folder := args[0]
 
-	pushFile(
-		folder,
-		"en",
-		fmt.Sprintf(
-			"https://www.transifex.com/api/2/project/%s/resource/%s/content/",
-			project,
-			resource,
-		),
-	)
+	pushFile(paths.New(folder, "en.po"))
 }
