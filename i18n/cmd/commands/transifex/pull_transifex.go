5fc2b04650e3d4f94e5cc0cd45634aeb2439a3a1
Silvano Cerza <silvanocerza@gmail.com>
2022-01-27 15:08:55 +0100

    Update i18n transifex pull command to use API v3

--- i18n/cmd/commands/transifex/pull_transifex.go
+++ i18n/cmd/commands/transifex/pull_transifex.go
@@ -16,13 +16,17 @@
 package transifex
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"path"
+	"sync"
+	"time"
 
+	"github.com/arduino/go-paths-helper"
 	"github.com/spf13/cobra"
 )
 
@@ -33,47 +37,212 @@ var pullTransifexCommand = &cobra.Command{
 }
 
 func getLanguages() []string {
+	url := mainEndpoint + fmt.Sprintf("projects/o:%s:p:%s/languages", organization, project)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	req.Header.Set("Content-Type", "application/vnd.api+json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	defer res.Body.Close()
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var jsonRes map[string]interface{}
+	if err := json.Unmarshal(b, &jsonRes); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var languages []string
+	data := jsonRes["data"].([]interface{})
+	for _, object := range data {
+		languageCode := object.(map[string]interface{})["attributes"].(map[string]interface{})["code"].(string)
+		languages = append(languages, languageCode)
+	}
+	return languages
+}
+
+// startTranslationDownload notifies Transifex that we want to start downloading
+// the resources file for the specified languageCode.
+// Returns an id to monitor the download status.
+func startTranslationDownload(languageCode string) string {
+	url := mainEndpoint + "resource_translations_async_downloads"
+
+	jsonData := map[string]interface{}{
+		"data": map[string]interface{}{
+			"relationships": map[string]interface{}{
+				"language": map[string]interface{}{
+					"data": map[string]string{
+						"id":   fmt.Sprintf("l:%s", languageCode),
+						"type": "languages",
+					},
+				},
+				"resource": map[string]interface{}{
+					"data": map[string]string{
+						"id":   fmt.Sprintf("o:%s:p:%s:r:%s", organization, project, resource),
+						"type": "resources",
+					},
+				},
+			},
+			"type": "resource_translations_async_downloads",
+		},
+	}
+
+	jsonBytes, err := json.Marshal(jsonData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	req, err := http.NewRequest(
-		"GET",
-		fmt.Sprintf(
-			"https://www.transifex.com/api/2/project/%s/resource/%s/stats/",
-			project, resource,
-		), nil)
-
+		"POST",
+		url,
+		bytes.NewBuffer(jsonBytes),
+	)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	req.SetBasicAuth("api", apiKey)
+	req.Header.Set("Content-Type", "application/vnd.api+json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
 
-	resp, err := http.DefaultClient.Do(req)
-
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	defer resp.Body.Close()
-
-	b, err := ioutil.ReadAll(resp.Body)
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	var jsonResp map[string]interface{}
-	if err := json.Unmarshal(b, &jsonResp); err != nil {
-		fmt.Println(err.Error())
+	var jsonRes map[string]interface{}
+	if err = json.Unmarshal(body, &jsonRes); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return jsonRes["data"].(map[string]interface{})["id"].(string)
+}
+
+// getDownloadURL checks for the download status of the languageCode file specified
+// by downloadID.
+// It return a URL to download the file when ready.
+func getDownloadURL(languageCode, downloadID string) string {
+	url := mainEndpoint + "resource_translations_async_downloads/" + downloadID
+	// The download request status must be asked from time to time, if it's
+	// still pending we try again using exponentional backoff starting from 2.5 seconds.
+	backoff := 2500 * time.Millisecond
+	for {
+
+		req, err := http.NewRequest(
+			"GET",
+			url,
+			nil,
+		)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		req.Header.Set("Content-Type", "application/vnd.api+json")
+		req.Header.Set("Authorization", "Bearer "+apiKey)
+
+		client := http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				// We handle redirection manually
+				return http.ErrUseLastResponse
+			},
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if res.StatusCode == 303 {
+			// Return the URL to download translation file
+			return res.Header.Get("location")
+		}
+
+		var body []byte
+		{
+			defer res.Body.Close()
+			body, err = io.ReadAll(res.Body)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		var jsonRes map[string]interface{}
+		if err = json.Unmarshal(body, &jsonRes); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		data := jsonRes["data"].(map[string]interface{})
+		attributes := data["attributes"].(map[string]interface{})
+		status := attributes["status"].(string)
+		switch status {
+		case "succeeded":
+			return ""
+		case "pending":
+			fallthrough
+		case "processing":
+			fmt.Printf("Current status for language %s: %s\n", languageCode, status)
+			time.Sleep(backoff)
+			backoff = backoff * 2
+			// Request the status again
+			continue
+		case "failed":
+			errs := attributes["errors"].([]map[string]string)
+			for _, err := range errs {
+				fmt.Printf("%s: %s\n", err["code"], err["detail"])
+			}
+			os.Exit(1)
+		}
+		fmt.Printf("Status request for language %s failed in an unforeseen way\n", languageCode)
+		os.Exit(1)
+	}
+}
+
+// download file from url and saves it in folder with the specified fileName
+func download(folder, fileName, url string) {
+	fmt.Printf("Starting download of %s\n", fileName)
+	filePath := paths.New(folder, fileName)
+
+	res, err := http.DefaultClient.Get(url)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	var langs []string
-	for key := range jsonResp {
-		langs = append(langs, key)
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	return langs
+	filePath.WriteFile(data)
+	fmt.Printf("Finished download of %s\n", fileName)
 }
 
 func pullCatalog(cmd *cobra.Command, args []string) {
@@ -82,50 +251,16 @@ func pullCatalog(cmd *cobra.Command, args []string) {
 
 	folder := args[0]
 
+	var wg sync.WaitGroup
 	for _, lang := range languages {
-
-		req, err := http.NewRequest(
-			"GET",
-			fmt.Sprintf(
-				"https://www.transifex.com/api/2/project/%s/resource/%s/translation/%s/?mode=reviewed&file=po",
-				project, resource, lang,
-			), nil)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		req.SetBasicAuth("api", apiKey)
-
-		resp, err := http.DefaultClient.Do(req)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		defer resp.Body.Close()
-
-		b, err := ioutil.ReadAll(resp.Body)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		os.Remove(path.Join(folder, lang+".po"))
-		file, err := os.OpenFile(path.Join(folder, lang+".po"), os.O_CREATE|os.O_RDWR, 0644)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		_, err = file.Write(b)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+		wg.Add(1)
+		go func(lang string) {
+			downloadID := startTranslationDownload(lang)
+			url := getDownloadURL(lang, downloadID)
+			download(folder, lang+".po", url)
+			wg.Done()
+		}(lang)
 	}
+	wg.Wait()
+	fmt.Println("Translation files downloaded")
 }
