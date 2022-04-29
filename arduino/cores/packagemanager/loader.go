10d95b230000cb5938ed8e44070299986b2cf16c
Cristian Maglie <c.maglie@arduino.cc>
2022-04-29 11:31:29 +0200

    Made packagemamager.loadToolsFromPackage public

--- arduino/cores/packagemanager/loader.go
+++ arduino/cores/packagemanager/loader.go
@@ -128,7 +128,7 @@ func (pm *PackageManager) LoadHardwareFromDirectory(path *paths.Path) []error {
 		toolsSubdirPath := packagerPath.Join("tools")
 		if toolsSubdirPath.IsDir() {
 			pm.Log.Infof("Checking existence of 'tools' path: %s", toolsSubdirPath)
-			merr = append(merr, pm.loadToolsFromPackage(targetPackage, toolsSubdirPath)...)
+			merr = append(merr, pm.LoadToolsFromPackageDir(targetPackage, toolsSubdirPath)...)
 		}
 		// If the Package does not contain Platforms or Tools we remove it since does not contain anything valuable
 		if len(targetPackage.Platforms) == 0 && len(targetPackage.Tools) == 0 {
@@ -589,7 +589,7 @@ func convertUploadToolsToPluggableDiscovery(props *properties.Map) {
 	props.Merge(propsToAdd)
 }
 
-func (pm *PackageManager) loadToolsFromPackage(targetPackage *cores.Package, toolsPath *paths.Path) []error {
+func (pm *PackageManager) LoadToolsFromPackageDir(targetPackage *cores.Package, toolsPath *paths.Path) []error {
 	pm.Log.Infof("Loading tools from dir: %s", toolsPath)
 
 	var merr []error
@@ -712,7 +712,7 @@ func (pm *PackageManager) LoadToolsFromBundleDirectory(toolsPath *paths.Path) er
 	} else {
 		// otherwise load the tools inside the unnamed package
 		unnamedPackage := pm.Packages.GetOrCreatePackage("")
-		pm.loadToolsFromPackage(unnamedPackage, toolsPath)
+		pm.LoadToolsFromPackageDir(unnamedPackage, toolsPath)
 	}
 	return nil
 }
