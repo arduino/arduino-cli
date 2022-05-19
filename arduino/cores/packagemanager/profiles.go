170382d995955fd8ecb3b2c98bfc5e27beabc966
Cristian Maglie <c.maglie@arduino.cc>
2022-05-19 13:34:08 +0200

    Remove stray comment

--- arduino/cores/packagemanager/profiles.go
+++ arduino/cores/packagemanager/profiles.go
@@ -83,9 +83,8 @@ func (pm *PackageManager) loadProfilePlatform(platformRef *sketch.ProfilePlatfor
 }
 
 func (pm *PackageManager) installMissingProfilePlatform(platformRef *sketch.ProfilePlatformReference, destDir *paths.Path, downloadCB rpc.DownloadProgressCB, taskCB rpc.TaskProgressCB) error {
-	// TODO: TaskProgressCB
-
 	// Instantiate a temporary package manager only for platform installation
+	_ = pm.TempDir.MkdirAll()
 	tmp, err := paths.MkTempDir(pm.TempDir.String(), "")
 	if err != nil {
 		return fmt.Errorf("installing missing platform: could not create temp dir %s", err)
