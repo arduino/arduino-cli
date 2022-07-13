9ae6760080000660bfcd5614cbf9722db86b4e25
Cristian Maglie <c.maglie@arduino.cc>
2022-07-13 16:22:05 +0200

    testsuite: added env commands to download and extract files

--- testsuite/env.go
+++ testsuite/env.go
@@ -16,9 +16,17 @@
 package integrationtest
 
 import (
+	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"net/http"
+	"net/url"
+	"path/filepath"
 	"testing"
 
 	"github.com/arduino/go-paths-helper"
+	"github.com/codeclysm/extract/v3"
 	"github.com/stretchr/testify/require"
 )
 
@@ -56,4 +64,66 @@ func (e *Environment) CleanUp() {
 // Root returns the root dir of the environment.
 func (e *Environment) Root() *paths.Path {
 	return e.rootDir
+}
+
+// Download downloads a file from a URL and returns the path to the downloaded file.
+// The file is saved and cached in a shared downloads directory. If the file already exists, it is not downloaded again.
+func (e *Environment) Download(rawURL string) *paths.Path {
+	url, err := url.Parse(rawURL)
+	e.t.NoError(err)
+
+	filename := filepath.Base(url.Path)
+	if filename == "/" {
+		filename = ""
+	} else {
+		filename = "-" + filename
+	}
+
+	hash := md5.Sum([]byte(rawURL))
+	resource := e.downloadsDir.Join(hex.EncodeToString(hash[:]) + filename)
+
+	// If the resource already exist, return it
+	if resource.Exist() {
+		return resource
+	}
+
+	// Download file
+	resp, err := http.Get(rawURL)
+	e.t.NoError(err)
+	defer resp.Body.Close()
+
+	// Copy data in a temp file
+	tmp := resource.Parent().Join(resource.Base() + ".tmp")
+	out, err := tmp.Create()
+	e.t.NoError(err)
+	defer tmp.Remove()
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	e.t.NoError(err)
+	e.t.NoError(out.Close())
+
+	// Rename the file to its final destination
+	e.t.NoError(tmp.Rename(resource))
+
+	return resource
+}
+
+// Extract extracts a tarball to a directory named as the archive
+// with the "_content" suffix added. Returns the path to the directory.
+func (e *Environment) Extract(archive *paths.Path) *paths.Path {
+	destDir := archive.Parent().Join(archive.Base() + "_content")
+	if destDir.Exist() {
+		return destDir
+	}
+
+	file, err := archive.Open()
+	e.t.NoError(err)
+	defer file.Close()
+
+	err = extract.Archive(context.Background(), file, destDir.String(), nil)
+	e.t.NoError(err)
+
+	return destDir
 }
