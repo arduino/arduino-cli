8e32390df12364b69a764863d3ab88fc049d83c6
Matteo Pologruto <m.pologruto@ext.arduino.cc>
2022-09-01 13:03:28 +0200

    Migrate TestCompileErrorMessage from test_compile_part_1.py to compile_part_1_test.go

--- internal/integrationtest/compile/compile_part_1_test.go
+++ internal/integrationtest/compile/compile_part_1_test.go
@@ -19,6 +19,7 @@ import (
 	"testing"
 
 	"github.com/arduino/arduino-cli/internal/integrationtest"
+	"github.com/arduino/go-paths-helper"
 	"github.com/stretchr/testify/require"
 )
 
@@ -37,4 +38,49 @@ func TestCompileWithoutFqbn(t *testing.T) {
 	// Build sketch without FQBN
 	_, _, err = cli.Run("compile")
 	require.Error(t, err)
+}
+
+func TestCompileErrorMessage(t *testing.T) {
+	env, cli := integrationtest.CreateArduinoCLIWithEnvironment(t)
+	defer env.CleanUp()
+
+	// Init the environment explicitly
+	_, _, err := cli.Run("core", "update-index")
+	require.NoError(t, err)
+
+	// Download latest AVR
+	_, _, err = cli.Run("core", "install", "arduino:avr")
+	require.NoError(t, err)
+
+	// Run a batch of bogus compile in a temp dir to check the error messages
+	tmp, err := paths.MkTempDir("", "tmp_dir")
+	require.NoError(t, err)
+	defer tmp.RemoveAll()
+	abcdef := tmp.Join("ABCDEF")
+	_, stderr, err := cli.Run("compile", "-b", "arduino:avr:uno", abcdef.String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "no such file or directory:")
+	_, stderr, err = cli.Run("compile", "-b", "arduino:avr:uno", abcdef.Join("ABCDEF.ino").String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "no such file or directory:")
+	_, stderr, err = cli.Run("compile", "-b", "arduino:avr:uno", abcdef.Join("QWERTY").String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "no such file or directory:")
+
+	err = abcdef.Mkdir()
+	require.NoError(t, err)
+	_, stderr, err = cli.Run("compile", "-b", "arduino:avr:uno", abcdef.String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "main file missing from sketch:")
+	_, stderr, err = cli.Run("compile", "-b", "arduino:avr:uno", abcdef.Join("ABCDEF.ino").String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "no such file or directory:")
+
+	qwertyIno := abcdef.Join("QWERTY.ino")
+	f, err := qwertyIno.Create()
+	require.NoError(t, err)
+	defer f.Close()
+	_, stderr, err = cli.Run("compile", "-b", "arduino:avr:uno", qwertyIno.String())
+	require.Error(t, err)
+	require.Contains(t, string(stderr), "main file missing from sketch:")
 }
