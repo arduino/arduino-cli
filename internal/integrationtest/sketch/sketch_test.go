e6b720d0e645f6a32fc3d417f21981da56a5bc66
Matteo Pologruto <m.pologruto@ext.arduino.cc>
2022-10-12 11:53:29 +0200

    Migrate TestSketchDotArgRelativeZipPathAndNameWithoutExtension from test_sketch.py to sketch_test.go

--- internal/integrationtest/sketch/sketch_test.go
+++ internal/integrationtest/sketch/sketch_test.go
@@ -143,3 +143,21 @@ func TestSketchDotArgAbsoluteZipPath(t *testing.T) {
 	defer require.NoError(t, archive.Close())
 	verifyZipContainsSketchExcludingBuildDir(t, archive.File)
 }
+
+func TestSketchArchiveDotArgRelativeZipPathAndNameWithoutExtension(t *testing.T) {
+	env, cli := integrationtest.CreateArduinoCLIWithEnvironment(t)
+	defer env.CleanUp()
+
+	// Creates a folder where to save the zip
+	archivesFolder := cli.WorkingDir().Join("my_archives")
+	require.NoError(t, archivesFolder.Mkdir())
+
+	cli.SetWorkingDir(cli.CopySketch("sketch_simple"))
+	_, _, err := cli.Run("sketch", "archive", ".", "../my_archives/my_custom_sketch")
+	require.NoError(t, err)
+
+	archive, err := zip.OpenReader(archivesFolder.Join("my_custom_sketch.zip").String())
+	require.NoError(t, err)
+	defer require.NoError(t, archive.Close())
+	verifyZipContainsSketchExcludingBuildDir(t, archive.File)
+}
