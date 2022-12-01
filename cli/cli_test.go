ecec6761c6dbc9d495cc6d6d52b7392c15a5a030
Cristian Maglie <c.maglie@arduino.cc>
2022-12-01 16:26:41 +0100

    Added source code static-check to enforce `--format` output.
    The first test run gives:
    
    === RUN   TestNoDirectOutputToStdOut
    burnbootloader/burnbootloader.go:85:5: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    burnbootloader/burnbootloader.go:85:16: object `os.Stderr` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:235:84: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:235:95: object `os.Stderr` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:274:72: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:274:83: object `os.Stderr` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:296:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:297:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:298:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:305:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:306:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:307:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:308:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:309:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:311:3: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:313:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:319:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:321:5: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:325:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    compile/compile.go:326:4: function `fmt.Print` should not be used in this package (use `feedback.*` instead)
    completion/completion.go:58:34: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    completion/completion.go:61:38: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    completion/completion.go:63:32: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    completion/completion.go:66:32: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    completion/completion.go:68:38: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    daemon/interceptors.go:29:19: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    debug/debug.go:106:82: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    monitor/term.go:38:9: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    output/rpc_progress.go:115:4: function `fmt.Print` should not be used in this package (use `feedback.*` instead)
    output/rpc_progress.go:117:5: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    output/rpc_progress.go:119:5: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    upload/upload.go:163:5: object `os.Stdout` should not be used in this package (use `feedback.*` instead)
    upload/upload.go:163:16: object `os.Stderr` should not be used in this package (use `feedback.*` instead)
    cli.go:215:4: function `fmt.Println` should not be used in this package (use `feedback.*` instead)
    --- FAIL: TestNoDirectOutputToStdOut (0.01s)
    FAIL
    exit status 1
    FAIL	github.com/arduino/arduino-cli/cli	0.019s

---
+++ cli/cli_test.go
@@ -0,0 +1,105 @@
+// This file is part of arduino-cli.
+//
+// Copyright 2022 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
+package cli_test
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
+	"testing"
+
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNoDirectOutputToStdOut(t *testing.T) {
+	dirs, err := paths.New(".").ReadDirRecursiveFiltered(
+		paths.FilterOutNames("testdata"), // skip all testdata folders
+		paths.AndFilter(
+			paths.FilterDirectories(),        // analyze only packages
+			paths.FilterOutNames("feedback"), // skip feedback package
+		))
+	require.NoError(t, err)
+	dirs.Add(paths.New("."))
+
+	for _, dir := range dirs {
+		testDir(t, dir)
+	}
+}
+
+func testDir(t *testing.T, dir *paths.Path) {
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, dir.String(), nil, parser.ParseComments)
+	require.NoError(t, err)
+
+	for _, pkg := range pkgs {
+		for _, file := range pkg.Files {
+			// Do not analyze test files
+			if strings.HasSuffix(expr(file.Name), "_test") {
+				continue
+			}
+
+			ast.Inspect(file, func(n ast.Node) bool {
+				return inspect(t, fset, n)
+			})
+		}
+	}
+}
+
+func inspect(t *testing.T, fset *token.FileSet, node ast.Node) bool {
+	switch n := node.(type) {
+	case *ast.CallExpr:
+		name := expr(n.Fun)
+		if strings.HasPrefix(name, "fmt.P") {
+			fmt.Printf("%s: function `%s` should not be used in this package (use `feedback.*` instead)\n", fset.Position(n.Pos()), name)
+			t.Fail()
+		}
+	case *ast.SelectorExpr:
+		wanted := map[string]bool{
+			"os.Stdout": true,
+			"os.Stderr": true,
+		}
+		name := expr(n)
+		if wanted[name] {
+			fmt.Printf("%s: object `%s` should not be used in this package (use `feedback.*` instead)\n", fset.Position(n.Pos()), name)
+			t.Fail()
+		}
+	}
+	return true
+}
+
+// expr returns the string representation of an expression, it doesn't expand function arguments or array index.
+func expr(_e ast.Expr) string {
+	switch e := _e.(type) {
+	case *ast.ArrayType:
+		return "[...]" + expr(e.Elt)
+	case *ast.CallExpr:
+		return expr(e.Fun) + "(...)"
+	case *ast.FuncLit:
+		return "func(...) {...}"
+	case *ast.SelectorExpr:
+		return expr(e.X) + "." + e.Sel.String()
+	case *ast.IndexExpr:
+		return expr(e.X) + "[...]"
+	case *ast.Ident:
+		return e.String()
+	default:
+		msg := fmt.Sprintf("UNKWOWN: %T", e)
+		panic(msg)
+	}
+}
