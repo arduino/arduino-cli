f176565d0b5763ee58ae49599197c9530f9203f4
zachary-walters <zachary.christopher.walters@gmail.com>
2023-01-31 08:20:13 -0600

    Removed the typo grave accent from ctags_parser_test

--- legacy/builder/ctags/ctags_parser_test.go
+++ legacy/builder/ctags/ctags_parser_test.go
@@ -11,7 +11,7 @@
 // a commercial license. Buying such a license is mandatory if you want to
 // modify or otherwise use the software for commercial activities involving the
 // Arduino software without disclosing the source code of your own applications.
-// To purchase a commercial license, send an email to license@arduino.c`c.
+// To purchase a commercial license, send an email to license@arduino.cc.
 
 package ctags
 
