8b37244b555044a55aab07ff108ad6643a7798d5
Cristian Maglie <c.maglie@arduino.cc>
2023-08-23 12:48:52 +0200

    Added integration tests for all default_profile cases

--- internal/integrationtest/profiles/profiles_test.go
+++ internal/integrationtest/profiles/profiles_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/arduino/arduino-cli/internal/integrationtest"
 	"github.com/stretchr/testify/require"
+	"go.bug.st/testifyjson/requirejson"
 )
 
 func TestCompileWithProfiles(t *testing.T) {
@@ -78,16 +79,74 @@ func TestCompileWithDefaultProfile(t *testing.T) {
 	_, _, err := cli.Run("core", "update-index")
 	require.NoError(t, err)
 
+	// Installa core/libs globally
+	_, _, err = cli.Run("core", "install", "arduino:avr@1.8.3")
+	require.NoError(t, err)
+
 	// copy sketch_with_profile into the working directory
-	sketchPath := cli.CopySketch("sketch_with_profile")
+	sketchWithoutDefProfilePath := cli.CopySketch("sketch_without_default_profile")
+	sketchWithDefProfilePath := cli.CopySketch("sketch_with_default_profile")
 
-	// compile using the default profile with its fqbn
-	stdout, _, err := cli.Run("compile", sketchPath.String())
-	require.NoError(t, err)
-	require.Contains(t, string(stdout), "arduino:avr")
+	{
+		// no default profile -> error missing FQBN
+		_, _, err := cli.Run("compile", sketchWithoutDefProfilePath.String(), "--format", "json")
+		require.Error(t, err)
+	}
+	{
+		// specified fbqn -> compile with specified FQBN and use global installation
+		stdout, _, err := cli.Run("compile", "-b", "arduino:avr:nano", sketchWithoutDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.3"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:nano" ]`)
+	}
+	{
+		// specified profile -> use the specified profile
+		stdout, _, err := cli.Run("compile", "--profile", "avr1", sketchWithoutDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.4"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:uno" ]`)
+	}
+	{
+		// specified profile and fqbn -> use the specified profile and override fqbn
+		stdout, _, err := cli.Run("compile", "--profile", "avr1", "-b", "arduino:avr:nano", sketchWithoutDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.4"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:nano" ]`)
+	}
 
-	// compile using a different fbqn -> should have priority over the one in the default profile
-	stdout, _, err = cli.Run("compile", "-b", "arduino:samd:mkr1000", sketchPath.String())
-	require.NoError(t, err)
-	require.Contains(t, string(stdout), "arduino:samd")
+	{
+		// default profile -> use default profile
+		stdout, _, err := cli.Run("compile", sketchWithDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.5"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:leonardo" ]`)
+	}
+	{
+		// default profile, specified fbqn -> use default profile, override fqbn
+		stdout, _, err := cli.Run("compile", "-b", "arduino:avr:nano", sketchWithDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.5"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:nano" ]`)
+	}
+	{
+		// default profile, specified different profile -> use the specified profile
+		stdout, _, err := cli.Run("compile", "--profile", "avr1", sketchWithDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.4"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:uno" ]`)
+	}
+	{
+		// default profile, specified different profile and fqbn -> use the specified profile and override fqbn
+		stdout, _, err := cli.Run("compile", "--profile", "avr1", "-b", "arduino:avr:nano", sketchWithDefProfilePath.String(), "--format", "json")
+		require.NoError(t, err)
+		jsonOut := requirejson.Parse(t, stdout)
+		jsonOut.Query(".builder_result.build_platform").MustContain(`{"id":"arduino:avr", "version":"1.8.4"}`)
+		jsonOut.Query(".builder_result.build_properties").MustContain(`[ "build.fqbn=arduino:avr:nano" ]`)
+	}
 }
