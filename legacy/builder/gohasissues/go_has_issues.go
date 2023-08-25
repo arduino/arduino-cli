54864ed2e11c18c77bc8fbe7d56137f9eb2810eb
Cristian Maglie <c.maglie@arduino.cc>
2023-08-25 17:34:25 +0200

    Removed no more needed functions

--- legacy/builder/gohasissues/go_has_issues.go
+++ legacy/builder/gohasissues/go_has_issues.go
@@ -19,69 +19,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	"sort"
 )
-
-func Walk(root string, walkFn filepath.WalkFunc) error {
-	info, err := os.Stat(root)
-	if err != nil {
-		return walkFn(root, nil, err)
-	}
-	return walk(root, info, walkFn)
-}
-
-func walk(path string, info os.FileInfo, walkFn filepath.WalkFunc) error {
-	err := walkFn(path, info, nil)
-	if err != nil {
-		if info.IsDir() && err == filepath.SkipDir {
-			return nil
-		}
-		return err
-	}
-
-	if !info.IsDir() {
-		return nil
-	}
-
-	names, err := readDirNames(path)
-	if err != nil {
-		return walkFn(path, info, err)
-	}
-
-	for _, name := range names {
-		filename := filepath.Join(path, name)
-		fileInfo, err := os.Stat(filename)
-		if err != nil {
-			if err := walkFn(filename, fileInfo, err); err != nil && err != filepath.SkipDir {
-				return err
-			}
-		} else {
-			err = walk(filename, fileInfo, walkFn)
-			if err != nil {
-				if !fileInfo.IsDir() || err != filepath.SkipDir {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-// readDirNames reads the directory named by dirname and returns
-// a sorted list of directory entries.
-func readDirNames(dirname string) ([]string, error) {
-	f, err := os.Open(dirname)
-	if err != nil {
-		return nil, err
-	}
-	names, err := f.Readdirnames(-1)
-	f.Close()
-	if err != nil {
-		return nil, err
-	}
-	sort.Strings(names)
-	return names, nil
-}
 
 func ReadDir(dirname string) ([]os.FileInfo, error) {
 	entries, err := os.ReadDir(dirname)
