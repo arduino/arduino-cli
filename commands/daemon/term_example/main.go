fcc5bbd88f498706889d6447655b24ad88a07bfd
Alessio Perugini <alessioper.98@gmail.com>
2023-09-25 12:43:34 +0200

    apply revive linter suggestion about superfluous-else

--- commands/daemon/term_example/main.go
+++ commands/daemon/term_example/main.go
@@ -40,39 +40,37 @@ func main() {
 	// Create and initialize a CLI instance
 	cli := commands.NewArduinoCoreServiceClient(conn)
 
-	var instance *commands.Instance
-	if resp, err := cli.Create(context.Background(), &commands.CreateRequest{}); err != nil {
+	resp, err := cli.Create(context.Background(), &commands.CreateRequest{})
+	if err != nil {
 		log.Fatal("Create:", err)
-	} else {
-		instance = resp.Instance
 	}
+	instance := resp.Instance
 
-	if respStream, err := cli.Init(context.Background(), &commands.InitRequest{Instance: instance}); err != nil {
+	respStream, err := cli.Init(context.Background(), &commands.InitRequest{Instance: instance})
+	if err != nil {
 		log.Fatal("Init:", err)
-	} else {
-		for {
-			resp, err := respStream.Recv()
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			if err != nil {
-				log.Fatal("Init:", err)
-			}
-			fmt.Println(resp)
+	}
+	for {
+		resp, err := respStream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
 		}
+		if err != nil {
+			log.Fatal("Init:", err)
+		}
+		fmt.Println(resp)
 	}
 
 	// List boards and take the first available port
-	var port *commands.Port
-	if resp, err := cli.BoardList(context.Background(), &commands.BoardListRequest{Instance: instance}); err != nil {
+	respList, err := cli.BoardList(context.Background(), &commands.BoardListRequest{Instance: instance})
+	if err != nil {
 		log.Fatal("BoardList:", err)
-	} else {
-		ports := resp.GetPorts()
-		if len(ports) == 0 {
-			log.Fatal("No port to connect!")
-		}
-		port = ports[0].Port
 	}
+	ports := respList.GetPorts()
+	if len(ports) == 0 {
+		log.Fatal("No port to connect!")
+	}
+	port := ports[0].Port
 	fmt.Println("Detected port:", port.Label, port.ProtocolLabel)
 
 	connectToPort(cli, instance, port)
