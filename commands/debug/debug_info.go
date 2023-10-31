0c0573f322c19e31b6619e07065a85703c4fe5e7
Cristian Maglie <c.maglie@arduino.cc>
2023-10-31 12:31:57 +0100

    debug: Enforce programmer selection (#2394)
    * debug: Enforce programmer selection
    
    * Fixed unit tests
    
    * Increase code coverage

--- commands/debug/debug_info.go
+++ commands/debug/debug_info.go
@@ -108,14 +108,15 @@ func getDebugProperties(req *rpc.GetDebugConfigRequest, pme *packagemanager.Expl
 		}
 	}
 
-	if req.GetProgrammer() != "" {
-		if p, ok := platformRelease.Programmers[req.GetProgrammer()]; ok {
-			toolProperties.Merge(p.Properties)
-		} else if refP, ok := referencedPlatformRelease.Programmers[req.GetProgrammer()]; ok {
-			toolProperties.Merge(refP.Properties)
-		} else {
-			return nil, &arduino.ProgrammerNotFoundError{Programmer: req.GetProgrammer()}
-		}
+	if req.GetProgrammer() == "" {
+		return nil, &arduino.MissingProgrammerError{}
+	}
+	if p, ok := platformRelease.Programmers[req.GetProgrammer()]; ok {
+		toolProperties.Merge(p.Properties)
+	} else if refP, ok := referencedPlatformRelease.Programmers[req.GetProgrammer()]; ok {
+		toolProperties.Merge(refP.Properties)
+	} else {
+		return nil, &arduino.ProgrammerNotFoundError{Programmer: req.GetProgrammer()}
 	}
 
 	var importPath *paths.Path
