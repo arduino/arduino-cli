2d77b39a01211719e32076a48ccf339e26247134
Alessio Perugini <alessioper.98@gmail.com>
2023-11-06 08:36:58 +0100

    fix: use rpc enum String method

--- internal/cli/lib/search.go
+++ internal/cli/lib/search.go
@@ -173,12 +173,12 @@ func (res librarySearchResult) String() string {
 
 	var out strings.Builder
 
-	if string(res.results.Status) == rpc.LibrarySearchStatus_name[int32(rpc.LibrarySearchStatus_LIBRARY_SEARCH_STATUS_FAILED)] {
+	if string(res.results.Status) == rpc.LibrarySearchStatus_LIBRARY_SEARCH_STATUS_FAILED.String() {
 		out.WriteString(tr("No libraries matching your search.\nDid you mean...\n"))
 	}
 
 	for _, lib := range results {
-		if string(res.results.Status) == rpc.LibrarySearchStatus_name[int32(rpc.LibrarySearchStatus_LIBRARY_SEARCH_STATUS_SUCCESS)] {
+		if string(res.results.Status) == rpc.LibrarySearchStatus_LIBRARY_SEARCH_STATUS_SUCCESS.String() {
 			out.WriteString(tr(`Name: "%s"`, lib.Name) + "\n")
 			if res.namesOnly {
 				continue
