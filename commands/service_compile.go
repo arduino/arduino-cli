1b6d3edb39f7458e596e4ca42fc64350115534e4
Cristian Maglie <c.maglie@arduino.cc>
2024-05-24 16:39:11 +0200

    [breaking] --build-cache-path now saves under 'cores' subdir instead of 'core'

--- commands/service_compile.go
+++ commands/service_compile.go
@@ -178,19 +178,23 @@ func (s *arduinoCoreServerImpl) Compile(req *rpc.CompileRequest, stream rpc.Ardu
 		s.settings.GetCompilationsBeforeBuildCachePurge(),
 		s.settings.GetBuildCacheTTL().Abs())
 
-	var coreBuildCachePath *paths.Path
-	if req.GetBuildCachePath() == "" {
-		coreBuildCachePath = paths.TempDir().Join("arduino", "cores")
-	} else {
-		buildCachePath, err := paths.New(req.GetBuildCachePath()).Abs()
+	var buildCachePath *paths.Path
+	if req.GetBuildCachePath() != "" {
+		p, err := paths.New(req.GetBuildCachePath()).Abs()
 		if err != nil {
 			return &cmderrors.PermissionDeniedError{Message: i18n.Tr("Cannot create build cache directory"), Cause: err}
 		}
-		if err := buildCachePath.MkdirAll(); err != nil {
-			return &cmderrors.PermissionDeniedError{Message: i18n.Tr("Cannot create build cache directory"), Cause: err}
-		}
-		coreBuildCachePath = buildCachePath.Join("core")
+		buildCachePath = p
+	} else if p, ok := s.settings.GetBuildCachePath(); ok {
+		buildCachePath = p
+	} else {
+		buildCachePath = paths.TempDir().Join("arduino")
 	}
+	if err := buildCachePath.MkdirAll(); err != nil {
+		return &cmderrors.PermissionDeniedError{Message: i18n.Tr("Cannot create build cache directory"), Cause: err}
+	}
+	coreBuildCachePath := buildCachePath.Join("cores")
+
 	var extraCoreBuildCachePaths paths.PathList
 	if len(req.GetBuildCacheExtraPaths()) == 0 {
 		extraCoreBuildCachePaths = s.settings.GetBuildCacheExtraPaths()
