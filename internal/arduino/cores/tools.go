0569dbb91d48027e78c187a312966d6d671a76ef
bits <bigbits@hackx.cc>
2024-09-06 09:39:18 +0800

    fix style

--- internal/arduino/cores/tools.go
+++ internal/arduino/cores/tools.go
@@ -127,19 +127,19 @@ func (tr *ToolRelease) RuntimeProperties() *properties.Map {
 }
 
 var (
-	regexpLinuxArm   = regexp.MustCompile("arm.*-linux-gnueabihf")
-	regexpLinuxArm64 = regexp.MustCompile("(aarch64|arm64)-linux-gnu")
+	regexpLinuxArm     = regexp.MustCompile("arm.*-linux-gnueabihf")
+	regexpLinuxArm64   = regexp.MustCompile("(aarch64|arm64)-linux-gnu")
 	regexpLinuxRiscv64 = regexp.MustCompile("riscv64-linux-gnu")
-	regexpLinux64    = regexp.MustCompile("x86_64-.*linux-gnu")
-	regexpLinux32    = regexp.MustCompile("i[3456]86-.*linux-gnu")
-	regexpWindows32  = regexp.MustCompile("i[3456]86-.*(mingw32|cygwin)")
-	regexpWindows64  = regexp.MustCompile("(amd64|x86_64)-.*(mingw32|cygwin)")
-	regexpMac64      = regexp.MustCompile("x86_64-apple-darwin.*")
-	regexpMac32      = regexp.MustCompile("i[3456]86-apple-darwin.*")
-	regexpMacArm64   = regexp.MustCompile("arm64-apple-darwin.*")
-	regexpFreeBSDArm = regexp.MustCompile("arm.*-freebsd[0-9]*")
-	regexpFreeBSD32  = regexp.MustCompile("i?[3456]86-freebsd[0-9]*")
-	regexpFreeBSD64  = regexp.MustCompile("amd64-freebsd[0-9]*")
+	regexpLinux64      = regexp.MustCompile("x86_64-.*linux-gnu")
+	regexpLinux32      = regexp.MustCompile("i[3456]86-.*linux-gnu")
+	regexpWindows32    = regexp.MustCompile("i[3456]86-.*(mingw32|cygwin)")
+	regexpWindows64    = regexp.MustCompile("(amd64|x86_64)-.*(mingw32|cygwin)")
+	regexpMac64        = regexp.MustCompile("x86_64-apple-darwin.*")
+	regexpMac32        = regexp.MustCompile("i[3456]86-apple-darwin.*")
+	regexpMacArm64     = regexp.MustCompile("arm64-apple-darwin.*")
+	regexpFreeBSDArm   = regexp.MustCompile("arm.*-freebsd[0-9]*")
+	regexpFreeBSD32    = regexp.MustCompile("i?[3456]86-freebsd[0-9]*")
+	regexpFreeBSD64    = regexp.MustCompile("amd64-freebsd[0-9]*")
 )
 
 func (f *Flavor) isExactMatchWith(osName, osArch string) bool {
