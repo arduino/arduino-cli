211a7ea66bf9dc592243407d4c314618222ffdb2
Cristian Maglie <c.maglie@arduino.cc>
2025-06-10 16:27:10 +0200

    Added integration tests

---
+++ internal/integrationtest/sketch/profiles_test.go
@@ -0,0 +1,67 @@
+// This file is part of arduino-cli.
+//
+// Copyright 2022-2025 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-cli.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
+package sketch_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/arduino/arduino-cli/internal/integrationtest"
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSketchProfileDump(t *testing.T) {
+	env, cli := integrationtest.CreateArduinoCLIWithEnvironment(t)
+	defer env.CleanUp()
+
+	sketch, err := paths.New("testdata", "SketchWithLibrary").Abs()
+	require.NoError(t, err)
+
+	_, _, err = cli.Run("core", "install", "arduino:avr@1.8.6")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit BusIO@1.17.1")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit GFX Library@1.12.1")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit SSD1306@2.5.14")
+	require.NoError(t, err)
+
+	// Check if the profile dump:
+	// - keeps libraries in the sketch with a relative path
+	// - keeps libraries outside the sketch with an absolute path
+	// - keeps libraries installed in the system with just the name and version
+	libOutside := sketch.Join("..", "MyLibOutside")
+	out, _, err := cli.Run("compile", "-b", "arduino:avr:uno",
+		"--library", sketch.Join("libraries", "MyLib").String(),
+		"--library", libOutside.String(),
+		"--dump-profile",
+		sketch.String())
+	require.NoError(t, err)
+	require.Equal(t, strings.TrimSpace(`
+profiles:
+  uno:
+    fqbn: arduino:avr:uno
+    platforms:
+      - platform: arduino:avr (1.8.6)
+    libraries:
+      - dir: libraries/MyLib
+      - dir: `+libOutside.String()+`
+      - Adafruit SSD1306 (2.5.14)
+      - Adafruit GFX Library (1.12.1)
+      - Adafruit BusIO (1.17.1)
+`), strings.TrimSpace(string(out)))
+}
