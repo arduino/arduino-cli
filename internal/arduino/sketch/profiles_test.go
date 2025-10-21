a77f39c54ae07143a49a88c102f4c0efdf06c76c
Cristian Maglie <c.maglie@arduino.cc>
2025-10-21 16:31:03 +0200

    Added support for git-libraries in profiles

--- internal/arduino/sketch/profiles_test.go
+++ internal/arduino/sketch/profiles_test.go
@@ -53,3 +53,26 @@ func TestProjectFileLoading(t *testing.T) {
 		require.Error(t, err)
 	}
 }
+
+func TestProjectFileLibraries(t *testing.T) {
+	sketchProj := paths.New("testdata", "profiles", "profile_with_libraries.yml")
+	proj, err := LoadProjectFile(sketchProj)
+	require.NoError(t, err)
+	require.Len(t, proj.Profiles, 1)
+	prof := proj.Profiles[0]
+	require.Len(t, prof.Libraries, 5)
+	require.Equal(t, "FlashStorage@1.2.3", prof.Libraries[0].String())
+	require.Equal(t, "@dir:/path/to/system/lib", prof.Libraries[1].String())
+	require.Equal(t, "@dir:path/to/sketch/lib", prof.Libraries[2].String())
+	require.Equal(t, "@git:https://github.com/username/HelloWorld.git#v2.13", prof.Libraries[3].String())
+	require.Equal(t, "@git:https://github.com/username/HelloWorld.git#v2.14", prof.Libraries[4].String())
+	require.Equal(t, "FlashStorage_1.2.3_e525d7c96b27788f", prof.Libraries[0].InternalUniqueIdentifier())
+	require.Panics(t, func() { prof.Libraries[1].InternalUniqueIdentifier() })
+	require.Panics(t, func() { prof.Libraries[2].InternalUniqueIdentifier() })
+	require.Equal(t, "git-github.com_username_HelloWorld.git_v2.13-0c146203", prof.Libraries[3].InternalUniqueIdentifier())
+	require.Equal(t, "git-github.com_username_HelloWorld.git_v2.14-49f5df7f", prof.Libraries[4].InternalUniqueIdentifier())
+
+	orig, err := sketchProj.ReadFile()
+	require.NoError(t, err)
+	require.Equal(t, string(orig), proj.AsYaml())
+}
