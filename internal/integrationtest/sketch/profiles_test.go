a8423abb59561c23d677576421947bceb21d1e9f
Ben Buchwald <sparkyb@gmail.com>
2025-10-21 05:27:30 -0400

    bugfix: Interpret local library paths relative to the sketch folder (#3006)
    * Interpret local library paths relative to the sketch folder
    
    Previously a local library path would be relative to the sketch folder if you
    passed a folder path to compile, but if you passed a path to a sketch .ino file,
    it would be considered relative to that. This change makes it so that relative
    paths will consistently be interpreted relative to the folder whether you
    specify a .ino file or not.
    
    * refactor: avoid double scanning of the sketch if profile is active
    
    ---------
    
    Co-authored-by: Cristian Maglie <c.maglie@arduino.cc>

--- internal/integrationtest/sketch/profiles_test.go
+++ internal/integrationtest/sketch/profiles_test.go
@@ -97,3 +97,75 @@ profiles:
 			{"name": "MyLibOutside", "install_dir": ` + string(libOutsideJson) + `}
 		]`)
 }
+
+func TestRelativeLocalLib(t *testing.T) {
+	env, cli := integrationtest.CreateArduinoCLIWithEnvironment(t)
+	t.Cleanup(env.CleanUp)
+
+	// Prepare the sketch with libraries
+	tmpDir, err := paths.MkTempDir("", "")
+	require.NoError(t, err)
+	t.Cleanup(func() { _ = tmpDir.RemoveAll })
+
+	sketchTemplate, err := paths.New("testdata", "SketchWithLibrary").Abs()
+	require.NoError(t, err)
+
+	sketch := tmpDir.Join("SketchWithLibrary")
+	libInside := sketch.Join("libraries", "MyLib")
+	err = sketchTemplate.CopyDirTo(sketch)
+	require.NoError(t, err)
+
+	libOutsideTemplate := sketchTemplate.Join("..", "MyLibOutside")
+	libOutside := sketch.Join("..", "MyLibOutside")
+	err = libOutsideTemplate.CopyDirTo(libOutside)
+	require.NoError(t, err)
+
+	// Install the required core and libraries
+	_, _, err = cli.Run("core", "install", "arduino:avr@1.8.6")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit BusIO@1.17.1", "--no-overwrite")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit GFX Library@1.12.1", "--no-overwrite")
+	require.NoError(t, err)
+	_, _, err = cli.Run("lib", "install", "Adafruit SSD1306@2.5.14", "--no-overwrite")
+	require.NoError(t, err)
+
+	// Check if the profile dump:
+	// - keeps libraries in the sketch with a relative path to the folder, not the .ino
+	out, _, err := cli.Run("compile", "-b", "arduino:avr:uno",
+		"--library", libInside.String(),
+		"--library", libOutside.String(),
+		"--dump-profile",
+		sketch.Join("SketchWithLibrary.ino").String())
+	require.NoError(t, err)
+	require.Equal(t, strings.TrimSpace(`
+profiles:
+  uno:
+    fqbn: arduino:avr:uno
+    platforms:
+      - platform: arduino:avr (1.8.6)
+    libraries:
+      - dir: libraries/MyLib
+      - dir: `+libOutside.String()+`
+      - Adafruit SSD1306 (2.5.14)
+      - Adafruit GFX Library (1.12.1)
+      - Adafruit BusIO (1.17.1)
+`), strings.TrimSpace(string(out)))
+
+	outRelative := strings.Replace(string(out), libOutside.String(), "../MyLibOutside", 1)
+
+	// Dump the profile in the sketch directory and compile with it again
+	err = sketch.Join("sketch.yaml").WriteFile([]byte(outRelative))
+	require.NoError(t, err)
+	out, _, err = cli.Run("compile", "-m", "uno", "--json", sketch.Join("SketchWithLibrary.ino").String())
+	require.NoError(t, err)
+	// Check if local libraries are picked up correctly
+	libInsideJson, _ := json.Marshal(libInside.String())
+	libOutsideJson, _ := json.Marshal(libOutside.String())
+	j := requirejson.Parse(t, out).Query(".builder_result.used_libraries")
+	j.MustContain(`
+		[
+			{"name": "MyLib", "install_dir": ` + string(libInsideJson) + `},
+			{"name": "MyLibOutside", "install_dir": ` + string(libOutsideJson) + `}
+		]`)
+}
